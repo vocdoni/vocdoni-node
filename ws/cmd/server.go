1198f1308d51bcaa53af66730fc749c820edcc02
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2019-05-09 17:50:18 +0200

    init ws

---
+++ ws/cmd/server.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"syscall"
+
+	"ws/epoll"
+
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+)
+
+var epoller *epoll.Epoll
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	// Upgrade connection
+	conn, _, _, err := ws.UpgradeHTTP(r, w)
+	if err != nil {
+		return
+	}
+	if err := epoller.Add(conn); err != nil {
+		log.Printf("Failed to add connection %v", err)
+		conn.Close()
+	}
+}
+
+func main() {
+	// Increase resources limitations
+	var rLimit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+	rLimit.Cur = rLimit.Max
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+
+	// Start epoll
+	var err error
+	epoller, err = epoll.MkEpoll()
+	if err != nil {
+		panic(err)
+	}
+
+	go Start()
+
+	http.HandleFunc("/", wsHandler)
+	if err := http.ListenAndServe("0.0.0.0:8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Start() {
+	for {
+		connections, err := epoller.Wait()
+		if err != nil {
+			log.Printf("Failed to epoll wait %v", err)
+			continue
+		}
+		for _, conn := range connections {
+			if conn == nil {
+				break
+			}
+			if method, _, err := wsutil.ReadClientData(conn); err != nil {
+				if err := epoller.Remove(conn); err != nil {
+					log.Printf("Failed to remove %v", err)
+				}
+				conn.Close()
+			} else {
+				log.Printf("Method: %s", string(method))
+			}
+		}
+	}
+}
