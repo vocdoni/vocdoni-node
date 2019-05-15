d29c7fd67b65a7ef362ed48a3bcf6095d4211d9d
jordipainan <jordi.pinyana.paga@gmail.com>
2019-05-15 13:57:40 +0200

    generator prints response

--- cmd/generator/generator.go
+++ cmd/generator/generator.go
@@ -112,12 +112,7 @@ func main() {
 
 	log.Printf("Finished initializing %d connections", len(conns))
 
-	dummyRequest := `{
-		"id": "req-2345679",
-		"request": {
-		  "method": "ping"
-		}
-	  }`
+	dummyRequest := `{"id": "req-2345679", "request": {"method": "ping"}}`
 
 	i := 0
 	for {
@@ -125,6 +120,7 @@ func main() {
 		case <-timer.C:
 			var jsonStr = makeEnvelope(makeBallot())
 			log.Printf("Conn %d sending message %s", i%*connections, jsonStr)
+			log.Printf("Conn %d sending message %s", i%*connections, dummyRequest)
 			log.Printf("%v", conns)
 			conn := conns[i%*connections]
 			if err := conn.WriteControl(websocket.PingMessage, nil, time.Now().Add(time.Second*5)); err != nil {
@@ -132,6 +128,12 @@ func main() {
 			}
 			conn.WriteMessage(websocket.TextMessage, []byte(jsonStr))
 			conn.WriteMessage(websocket.TextMessage, []byte(dummyRequest))
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Printf("Cannot read message")
+			}
+			fmt.Println("Response message type: ", msgType)
+			fmt.Println("Response message content: ", string(msg))
 			i++
 			log.Printf("Sent!")
 		default:
