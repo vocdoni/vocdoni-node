0631814f5749102b20bc242c90ac240f7da228f4
imw <isaac@iwilder.net>
2019-05-16 10:38:11 +0200

    implements remaining api methods

--- net/router.go
+++ net/router.go
@@ -38,8 +38,20 @@ var fetchResponseFmt = `{
 var addResponseFmt = `{
 	"uri": "%s",
 	"request": "%s",
-	"timestamp": "%s",
+	"timestamp": %s,
 }`
+
+var listPinsResponseFmt = `{
+    "files": %s,
+    "request": "%s",
+    "timestamp": %s 
+  }`
+
+var boolResponseFmt = `{
+    "ok": %s,
+    "request": "%s",
+    "timestamp": %s
+  }`
 
 func failBody(requestId string, failString string) []byte {
 	return []byte(fmt.Sprintf(failBodyFmt, requestId, failString))
@@ -63,7 +75,8 @@ func Route(inbound <-chan types.Message, outbound chan<- types.Message, errorCha
 	for {
 		select {
 		case msg := <-inbound:
-			//probably from here to the switch should be factored out, error checking added
+
+			//probably from here to the switch should be factored out
 			var msgJSON interface{}
 			err := json.Unmarshal(msg.Data, &msgJSON)
 			if err != nil {
@@ -78,11 +91,13 @@ func Route(inbound <-chan types.Message, outbound chan<- types.Message, errorCha
 			if !ok {
 				log.Printf("No ID field in message or malformed")
 			}
+
 			requestMap, ok := msgMap["request"].(map[string]interface{})
 			if !ok {
 				log.Printf("No request field in message or malformed")
 			}
 			method := fmt.Sprintf("%v", requestMap["method"].(string))
+
 			switch method {
 			case "ping":
 				wsTransport.Send(buildReply(msg, []byte("pong")), errorChan)
@@ -90,6 +105,7 @@ func Route(inbound <-chan types.Message, outbound chan<- types.Message, errorCha
 				uri, ok := requestMap["uri"].(string)
 				if !ok {
 					log.Printf("No uri in fetchFile request or malformed")
+					//err to errchan, reply
 				}
 				content, err := storage.Retrieve(uri)
 				if err != nil {
@@ -101,31 +117,40 @@ func Route(inbound <-chan types.Message, outbound chan<- types.Message, errorCha
 				wsTransport.Send(buildReply(msg, successBody(requestId, fmt.Sprintf(fetchResponseFmt, b64content, requestId, time.Now().String()))), errorChan)
 				log.Printf("%v", content)
 			case "addFile":
-				reqType, ok := requestMap["type"].(string)
-				if !ok {
-					log.Printf("No type field in addFile request or malformed")
-				}
 				content, ok := requestMap["content"].(string)
 				if !ok {
 					log.Printf("No content field in addFile request or malformed")
+					//err to errchan, reply
+					break
 				}
 				b64content, err := base64.StdEncoding.DecodeString(content)
 				if err != nil {
 					log.Printf("Couldn't decode content")
+					//err to errchan, reply
 					break
 				}
 				name, ok := requestMap["name"].(string)
 				if !ok {
 					log.Printf("No name field in addFile request or malformed")
+					//err to errchan, reply
+					break
 				}
 				timestamp, errAtoi := strconv.ParseInt(requestMap["timestamp"].(string), 10, 64)
 				if errAtoi != nil {
 					log.Printf("timestamp wrong format")
+					//err to errchan, reply
+					break
+				}
+				reqType, ok := requestMap["type"].(string)
+				if !ok {
+					log.Printf("No type field in addFile request or malformed")
+					//err to errchan, reply
 					break
 				}
 				switch reqType {
 				case "swarm":
 					// TODO: Only need IPFS for now
+					//err to errchan, reply
 					break
 				case "ipfs":
 					cid, err := storage.Publish(b64content)
@@ -134,15 +159,44 @@ func Route(inbound <-chan types.Message, outbound chan<- types.Message, errorCha
 					}
 					log.Printf("added %s with name %s and with timestamp %s", cid, name, timestamp)
 					wsTransport.Send(buildReply(msg, successBody(requestId, fmt.Sprintf(addResponseFmt, cid, requestId, time.Now().String()))), errorChan)
-					break
 				}
 				//data.Publish
 			case "pinList":
+				pins, err := storage.ListPins()
+				if err != nil {
+					log.Printf("Internal error fetching pins")
+				}
+				pinsJsonArray, err := json.Marshal(pins)
+				if err != nil {
+					log.Printf("Internal error parsing pins")
+				}
+				wsTransport.Send(buildReply(msg, successBody(requestId, fmt.Sprintf(listPinsResponseFmt, pinsJsonArray, requestId, time.Now()))), errorChan)
+
 				//data.Pins
 			case "pinFile":
-				//data.Pin
+				uri, ok := requestMap["uri"].(string)
+				if !ok {
+					log.Printf("No uri in pinFile request or malformed")
+				}
+				err := storage.Pin(uri)
+				if err != nil {
+					failString := fmt.Sprintf("Error pinning file %s", requestMap["uri"])
+					errorChan <- errors.New(failString)
+					wsTransport.Send(buildReply(msg, failBody(requestId, failString)), errorChan)
+				}
+				wsTransport.Send(buildReply(msg, successBody(requestId, fmt.Sprintf(boolResponseFmt, "true", requestId, time.Now().String()))), errorChan)
 			case "unpinFile":
-				//data.Unpin
+				uri, ok := requestMap["uri"].(string)
+				if !ok {
+					log.Printf("No uri in unpinFile request or malformed")
+				}
+				err := storage.Pin(uri)
+				if err != nil {
+					failString := fmt.Sprintf("Error unpinning file %s", requestMap["uri"])
+					errorChan <- errors.New(failString)
+					wsTransport.Send(buildReply(msg, failBody(requestId, failString)), errorChan)
+				}
+				wsTransport.Send(buildReply(msg, successBody(requestId, fmt.Sprintf(boolResponseFmt, "true", requestId, time.Now().String()))), errorChan)
 			}
 		}
 	}
