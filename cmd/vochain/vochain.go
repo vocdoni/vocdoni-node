1c30bee5fea19d822fc8e24437c54eb4c9c6d404
div dev doe <divdevdoe@riseup.net>
2019-07-25 14:18:37 +0200

    Removes unecessary files and code

--- cmd/vochain/vochain.go
+++ cmd/vochain/vochain.go
@@ -5,21 +5,6 @@ import (
 )
 
 func main() {
-	//ctx := mock.NewDefaultContext()
-	//mock.StartInProcess(ctx, "counterApp", filepath.Join(ctx.Config.RootDir, "tendermint_data"))
 	vochain.InitTendermint()
 	vochain.StartTendermint()
-	//_, err := mock.startInProcess(ctx)
-	//app := mock.NewVotingApplication(false)
-	//initc := app.InitChain(types.RequestInitChain{})
-	//log.Infof("%+v", initc)
-	/*
-		for i := 0; i < 10; i++ {
-			app.Commit()
-		}
-	*/
-	//for {
-	//app.Commit()
-	//}
-	//log.Infof("%+v", app.Info(types.RequestInfo{}))
 }
