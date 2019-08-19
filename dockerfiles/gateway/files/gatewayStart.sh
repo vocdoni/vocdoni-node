18b5bd2854d345a9ed50c7fd5732b33881ae7b7c
imw <isaac@iwilder.net>
2019-08-19 14:51:33 +0200

    adds cluster params to docker script

--- dockerfiles/gateway/files/gatewayStart.sh
+++ dockerfiles/gateway/files/gatewayStart.sh
@@ -8,17 +8,20 @@ GWARGS="\
  ${GW_SSL_DOMAIN:+ --sslDomain ${GW_SSL_DOMAIN}}\
  ${GW_ALLOW_PRIVATE:+ --allowPrivate}\
  ${GW_ALLOWED_ADDRS:+ --allowedAddrs ${GW_ALLOWED_ADDRS}}\
- ${GW_DVOTE_ENABLED:+ --dvoteApi}\
  ${GW_LISTEN_HOST:+ --listenHost ${GW_LISTEN_HOST}}\
  ${GW_LISTEN_PORT:+ --listenPort ${GW_LISTEN_PORT}}\
- ${GW_DVOTE_PATH:+ --dvoteRoute ${GW_DVOTE_PATH}}\
+ ${GW_FILE_ENABLED:+ --fileApi=${GW_FILE_ENABLED}}\
+ ${GW_FILE_ROUTE:+ --fileRoute=${GW_FILE_ROUTE}}\
+ ${GW_CENSUS_ENABLED:+ --censusApi=${GW_CENSUS_ENABLED}}\
+ ${GW_CENSUS_ROUTE:+ --censusRoute=${GW_CENSUS_ROUTE}}\
  ${GW_W3_ENABLED:+ --web3Api}\
  ${GW_W3_EXTERNAL:+ --w3external ${GW_W3_EXTERNAL}}\
  ${GW_W3_ROUTE:+ --w3route ${GW_W3_ROUTE}}\
  ${GW_W3node_PORT:+ --w3nodePort ${GW_W3node_PORT}}\
  ${GW_W3_CHAIN:+ --chain ${GW_W3_CHAIN}}\
- ${GW_IPFS_DAEMON_PATH:+ --ipfsDaemon ${GW_IPFS_DAEMON_PATH}}\
- ${GW_IPFS_NO_INIT:+ --ipfsNoInit}\
+ ${GW_IPFS_NOINIT:+ --ipfsNoInit=${GW_IPFS_NOINIT}}\
+ ${GW_IPFS_CLUSTER_KEY:+ --ipfsClusterKey ${GW_IPFS_CLUSTER_KEY}}\
+ ${GW_IPFS_CLUSTER_PEERS:+ --ipfsClusterPeers ${GW_IPFS_CLUSTER_PEERS}}\
  "
 
 CMD="/app/gateway $GWARGS $@"
