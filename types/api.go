01bcb143e8dde7273b564b761cbb5631931f46ae
p4u <pau@dabax.net>
2019-11-25 19:55:21 +0100

    Fix PubKeys API definition

--- types/api.go
+++ types/api.go
@@ -11,24 +11,25 @@ type RequestMessage struct {
 /*MetaRequest contains all of the possible request fields.
 Fields must be in alphabetical order */
 type MetaRequest struct {
-	CensusID   string   `json:"censusId,omitempty"`
-	CensusURI  string   `json:"censusUri,omitempty"`
-	ClaimData  string   `json:"claimData,omitempty"`
-	ClaimsData []string `json:"claimsData,omitempty"`
-	Content    string   `json:"content,omitempty"`
-	From       int64    `json:"from,omitempty"`
-	ListSize   int64    `json:"listSize,omitempty"`
-	Method     string   `json:"method"`
-	Name       string   `json:"name,omitempty"`
-	Nullifier  string   `json:"nullifier,omitempty"`
-	Payload    *VoteTx  `json:"payload,omitempty"`
-	ProcessID  string   `json:"processId,omitempty"`
-	PubKeys    []string `json:"encryptionPublicKeys,omitempty"`
-	RootHash   string   `json:"rootHash,omitempty"`
-	Timestamp  int32    `json:"timestamp"`
-	Type       string   `json:"type,omitempty"`
-	URI        string   `json:"uri,omitempty"`
-	Signature  string   `json:"signature,omitempty"`
+	CensusID       string   `json:"censusId,omitempty"`
+	CensusURI      string   `json:"censusUri,omitempty"`
+	ClaimData      string   `json:"claimData,omitempty"`
+	ClaimsData     []string `json:"claimsData,omitempty"`
+	Content        string   `json:"content,omitempty"`
+	From           int64    `json:"from,omitempty"`
+	ListSize       int64    `json:"listSize,omitempty"`
+	Method         string   `json:"method"`
+	Name           string   `json:"name,omitempty"`
+	Nullifier      string   `json:"nullifier,omitempty"`
+	Payload        *VoteTx  `json:"payload,omitempty"`
+	ProcessID      string   `json:"processId,omitempty"`
+	PubKeys        []string `json:"pubKeys,omitempty"`
+	RootHash       string   `json:"rootHash,omitempty"`
+	Signature      string   `json:"signature,omitempty"`
+	Timestamp      int32    `json:"timestamp"`
+	Type           string   `json:"type,omitempty"`
+	URI            string   `json:"uri,omitempty"`
+	VotePublicKeys []string `json:"encryptionPublicKeys,omitempty"`
 }
 
 //ResponseMessage wraps an api response
