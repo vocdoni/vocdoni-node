56c8d4a71a6384145b8ebd7dd77c2c2d2afccfc7
Daniel Martí <mvdan@mvdan.cc>
2019-12-17 15:38:39 +0000

    rewrite net.WebsocketHandle to handle many clients
    The previous implementation read messages from connected clients in a
    synchronous round-robin fashion. This meant that if A and B both connect
    in that order, we would not read a message from B until we've read a
    message from A.
    
    Instead, read messages from each client in a separate goroutine, as they
    connect. Use an internal channel and goroutine to then route all of
    those messages to the receiver.
    
    This also means we don't need the client pool with its channels and map;
    each connected client is simply one more goroutine, since HTTP handlers
    are called in new goroutines anyway.

--- net/websockets.go
+++ net/websockets.go
@@ -12,49 +12,13 @@ import (
 	"gitlab.com/vocdoni/go-dvote/types"
 )
 
-type Client struct {
-	ID   string
-	Conn *websocket.Conn
-	Pool *clientPool
-}
-
-type clientPool struct {
-	Clients    map[*Client]bool
-	Register   chan *Client
-	Unregister chan *Client
-}
-
-func newClientPool() *clientPool {
-	return &clientPool{
-		Clients:    make(map[*Client]bool),
-		Register:   make(chan *Client),
-		Unregister: make(chan *Client),
-	}
-}
-
-func (p *clientPool) Start() {
-	for {
-		select {
-		case client := <-p.Register:
-			p.Clients[client] = true
-			log.Infof("client with id: %s connected", client.ID)
-			log.Debugf("client pool size: %d", len(p.Clients))
-			break
-		case client := <-p.Unregister:
-			delete(p.Clients, client)
-			log.Infof("client with id: %s disconnected", client.ID)
-			log.Debugf("client pool size: %d", len(p.Clients))
-			break
-		}
-	}
-}
-
 // WebsocketHandle represents the information required to work with ws in go-dvote
 type WebsocketHandle struct {
 	Connection *types.Connection // the ws connection
 	WsProxy    *Proxy            // proxy where the ws will be associated
 	Upgrader   *websocket.Upgrader
-	Pool       *clientPool
+
+	internalReceiver chan types.Message
 }
 
 // SetProxy sets the proxy for the ws
@@ -80,45 +44,53 @@ func (w *WebsocketHandle) Init(c *types.Connection) error {
 		WriteBufferSize: 4096,
 		CheckOrigin:     func(r *http.Request) bool { return true },
 	}
-
-	w.Pool = newClientPool()
-	go w.Pool.Start()
-
+	w.internalReceiver = make(chan types.Message, 1)
 	return nil
 }
 
-func (w *WebsocketHandle) Upgrade(responseWriter http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
-	conn, err := w.Upgrader.Upgrade(responseWriter, r, nil)
-	if err != nil {
-		log.Infof("cannot upgrade http connection to ws: %s", err)
-		return nil, err
+func (w *WebsocketHandle) Listen(receiver chan<- types.Message) {
+	// TODO(mvdan): this extra channel and goroutine is a tad unnecessary.
+	// Consider refactoring the interface.
+	for {
+		msg := <-w.internalReceiver
+		receiver <- msg
 	}
-
-	return conn, nil
 }
 
 // AddProxyHandler adds a handler on the proxy and upgrades the connection
 // a ws connection is activated with a normal http request with Connection: upgrade
 func (w *WebsocketHandle) AddProxyHandler(path string) {
 	ssl := w.WsProxy.C.SSLDomain != ""
-	serveWs := func(writer http.ResponseWriter, reader *http.Request) {
+	serveWs := func(resp http.ResponseWriter, req *http.Request) {
 		// Upgrade connection
-		conn, err := w.Upgrade(writer, reader)
+		conn, err := w.Upgrader.Upgrade(resp, req, nil)
 		if err != nil {
+			log.Errorf("cannot upgrade http connection to ws: %s", err)
 			return
 		}
 
-		client := &Client{
-			Conn: conn,
-			Pool: w.Pool,
-			ID:   time.Now().String(),
+		resp.Header().Set("Access-Control-Allow-Origin", "*")
+		resp.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+		resp.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
+
+		// Read websocket messages until the connection is closed. HTTP
+		// handlers are run in new goroutines, so we don't need to spawn
+		// another goroutine.
+		for {
+			_, payload, err := conn.ReadMessage()
+			if err != nil {
+				conn.Close()
+				break
+			}
+			msg := types.Message{
+				Data:      []byte(payload),
+				TimeStamp: int32(time.Now().Unix()),
+				Context: &WebsocketContext{
+					Conn: conn,
+				},
+			}
+			w.internalReceiver <- msg
 		}
-
-		w.Pool.Register <- client
-
-		writer.Header().Set("Access-Control-Allow-Origin", "*")
-		writer.Header().Set("Access-Control-Allow-Methods", "POST, GET")
-		writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
 	}
 	w.WsProxy.AddHandler(path, serveWs)
 
@@ -126,31 +98,6 @@ func (w *WebsocketHandle) AddProxyHandler(path string) {
 		log.Infof("ws initialized on ws://" + w.WsProxy.C.Address + ":" + strconv.Itoa(w.WsProxy.C.Port))
 	} else {
 		log.Infof("wss initialized on wss://" + w.WsProxy.C.SSLDomain + ":" + strconv.Itoa(w.WsProxy.C.Port))
-	}
-}
-
-// Listen listens for incoming data
-func (w *WebsocketHandle) Listen(reciever chan<- types.Message) {
-	var msg types.Message
-
-	for {
-		for client, added := range w.Pool.Clients {
-			if added == false {
-				break
-			}
-			_, payload, err := client.Conn.ReadMessage()
-			if err != nil {
-				w.Pool.Unregister <- client
-				client.Conn.Close()
-				continue
-			}
-			msg.Data = []byte(payload)
-			msg.TimeStamp = int32(time.Now().Unix())
-			ctx := new(WebsocketContext)
-			ctx.Conn = client.Conn
-			msg.Context = ctx
-			reciever <- msg
-		}
 	}
 }
 
