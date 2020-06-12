5f64d7d8f8ce65ecef774109994ff7ef549268ea
p4u <pau@dabax.net>
2020-06-12 15:08:07 +0200

    ethevents: if no peers available when resolving the contract ENS, retry
    Signed-off-by: p4u <pau@dabax.net>

--- service/ethevents.go
+++ service/ethevents.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"fmt"
+	"strings"
+	"time"
 
 	"gitlab.com/vocdoni/go-dvote/census"
 	"gitlab.com/vocdoni/go-dvote/chain"
@@ -12,6 +14,7 @@ import (
 )
 
 const ensRegistryAddr = "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"
+const maxRetries = 30
 
 // EthEvents service registers on the Ethereum smart contract specified in ethProcDomain, the provided event handlers
 // we3host and w3port must point to a working web3 websocket endpoint.
@@ -20,15 +23,10 @@ func EthEvents(ethProcDomain, w3uri string, startBlock int64, endBlock int64,
 	cm *census.Manager, signer *ethereum.SignKeys, vocapp *vochain.BaseApplication, evh []ethevents.EventHandler) error {
 	// TO-DO remove cm (add it on the eventHandler instead)
 	log.Infof("creating ethereum events service")
-
-	contractAddr, err := chain.VotingProcessAddress(
-		ensRegistryAddr, ethProcDomain, w3uri)
-	if err != nil || contractAddr == "" {
-		return fmt.Errorf("cannot get voting process contract: %s", err)
-	} else {
-		log.Infof("loaded voting contract at address: %s", contractAddr)
+	contractAddr, err := ensResolve(ensRegistryAddr, ethProcDomain, w3uri)
+	if err != nil {
+		return err
 	}
-
 	ev, err := ethevents.NewEthEvents(contractAddr, signer, w3uri, cm, vocapp)
 	if err != nil {
 		return fmt.Errorf("couldn't create ethereum events listener: (%s)", err)
@@ -45,4 +43,21 @@ func EthEvents(ethProcDomain, w3uri string, startBlock int64, endBlock int64,
 	}()
 
 	return nil
+}
+
+func ensResolve(ensRegistryAddr, ethDomain, w3uri string) (contractAddr string, err error) {
+	for i := 0; i < maxRetries; i++ {
+		contractAddr, err = chain.VotingProcessAddress(ensRegistryAddr, ethDomain, w3uri)
+		if err != nil {
+			if strings.Contains(err.Error(), "no suitable peers available") {
+				time.Sleep(time.Second)
+				continue
+			}
+			err = fmt.Errorf("cannot get voting process contract: %s", err)
+			return
+		}
+		log.Infof("loaded voting contract at address: %s", contractAddr)
+		break
+	}
+	return
 }
