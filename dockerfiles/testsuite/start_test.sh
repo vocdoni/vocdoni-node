c91453ce6656a007a5f85ad8550611310c1882a9
vdo <vdo@greyfaze.net>
2020-06-25 14:50:47 +0200

    Reduce logging

--- dockerfiles/testsuite/start_test.sh
+++ dockerfiles/testsuite/start_test.sh
@@ -3,18 +3,15 @@
 echo "### Starting test suite ###"
 docker-compose up -d
 
-echo "### Pre run logs ###"
-docker-compose logs
-
 echo "### Waiting for test suite to be ready ###"
 for i in {1..5}; do docker-compose run test curl --fail http://gateway:9090/ping && break || sleep 5; done
 
 echo "### Running test ###"
-docker-compose run test timeout 300 ./vochaintest --oracleKey=$TESTSUITE_ORACLE_KEY --electionSize=$TESTSUITE_ELECTION_SIZE --gwHost ws://gateway:9090/dvote
+docker-compose run test timeout 300 ./vochaintest --oracleKey=$TESTSUITE_ORACLE_KEY --electionSize=$TESTSUITE_ELECTION_SIZE --gwHost ws://gateway:9090/dvote --logLevel=INFO
 RETVAL=$?
 
 echo "### Post run logs ###"
-docker-compose logs --tail 50
+docker-compose logs --tail 300
 
 echo "### Cleaning environment ###"
 docker-compose down -v --remove-orphans
