8457e28f46035a2cf9f2354f1c232be853f400d3
p4u <pau@dabax.net>
2020-07-04 12:16:51 +0200

    vochain: add new unit test for the tendermint APP (checkTx, deliverTx and commit)
    Signed-off-by: p4u <pau@dabax.net>

---
+++ vochain/app_test.go
@@ -0,0 +1,137 @@
+package vochain
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"testing"
+
+	abcitypes "github.com/tendermint/tendermint/abci/types"
+	"gitlab.com/vocdoni/go-dvote/crypto/ethereum"
+	"gitlab.com/vocdoni/go-dvote/crypto/snarks"
+	"gitlab.com/vocdoni/go-dvote/db"
+	"gitlab.com/vocdoni/go-dvote/tree"
+	"gitlab.com/vocdoni/go-dvote/types"
+)
+
+func TestCheckTX(t *testing.T) {
+	app, err := NewBaseApplication(tempDir(t, "vochain_checkTxTest"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	treeStorage, err := db.NewIden3Storage(tempDir(t, "vochain_checkTxTest_db"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tr, err := tree.NewTree(treeStorage)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	keys := createEthRandomKeysBatch(t, 1000)
+	claims := []string{}
+	for _, k := range keys {
+		pub, _ := k.HexString()
+		pub, err = ethereum.DecompressPubKey(pub)
+		if err != nil {
+			t.Fatal(err)
+		}
+		pubb, err := hex.DecodeString(pub)
+		if err != nil {
+			t.Fatal(err)
+		}
+		c := snarks.Poseidon.Hash(pubb)
+		tr.AddClaim(c, nil)
+		claims = append(claims, string(c))
+	}
+	process := &types.Process{
+		StartBlock:     0,
+		Type:           types.PollVote,
+		EntityID:       randomHex(entityIDsize),
+		MkRoot:         tr.Root(),
+		NumberOfBlocks: 1024,
+	}
+	pid := randomHex(processIDsize)
+	t.Logf("adding process %+v", process)
+	app.State.AddProcess(process, pid, "ipfs://123456789")
+
+	var cktx abcitypes.RequestCheckTx
+	var detx abcitypes.RequestDeliverTx
+
+	var cktxresp abcitypes.ResponseCheckTx
+	var detxresp abcitypes.ResponseDeliverTx
+
+	var tx types.VoteTx
+	var proof string
+
+	for i, s := range keys {
+		proof, err = tr.GenProof([]byte(claims[i]), nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		tx = types.VoteTx{
+			Nonce:     randomHex(16),
+			ProcessID: pid,
+			Proof:     proof,
+		}
+
+		txBytes, err := json.Marshal(tx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if tx.Signature, err = s.Sign(txBytes); err != nil {
+			t.Fatal(err)
+		}
+		tx.Type = "vote"
+		if txBytes, err = json.Marshal(tx); err != nil {
+			t.Fatal(err)
+		}
+		cktx.Tx = txBytes
+		cktxresp = app.CheckTx(cktx)
+		if cktxresp.Code != 0 {
+			t.Fatalf(fmt.Sprintf("checkTX failed: %s", cktxresp.Data))
+		}
+		detx.Tx = txBytes
+		detxresp = app.DeliverTx(detx)
+		if detxresp.Code != 0 {
+			t.Fatalf(fmt.Sprintf("deliverTX failed: %s", detxresp.Data))
+		}
+		app.Commit()
+	}
+
+}
+
+func tempDir(tb testing.TB, name string) string {
+	tb.Helper()
+	dir, err := ioutil.TempDir("", name)
+	if err != nil {
+		tb.Fatal(err)
+	}
+	tb.Cleanup(func() { os.RemoveAll(dir) })
+	return dir
+}
+
+func randomHex(n int) string {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return ""
+	}
+	return hex.EncodeToString(bytes)
+}
+
+// CreateEthRandomKeysBatch creates a set of eth random signing keys
+func createEthRandomKeysBatch(tb testing.TB, n int) []*ethereum.SignKeys {
+	s := make([]*ethereum.SignKeys, n)
+	for i := 0; i < n; i++ {
+		s[i] = new(ethereum.SignKeys)
+		if err := s[i].Generate(); err != nil {
+			tb.Fatal(err)
+		}
+	}
+	return s
+}
