2a2b75688edf01923afe06bbcc21d2536e4c7596
p4u <pau@dabax.net>
2020-09-15 16:02:27 +0200

    update Production Genesis
    reset production blockchain in order to start the Early Access from scratch
    
    Signed-off-by: p4u <pau@dabax.net>

--- vochain/genesis.go
+++ vochain/genesis.go
@@ -3,111 +3,128 @@ package vochain
 // Testnet Genesis File for Vocdoni KISS v1
 const (
 	ReleaseGenesis1 = `
-{
-  "genesis_time": "2020-07-12T09:30:50.199392102Z",
-  "chain_id": "vocdoni-release-05",
-  "consensus_params": {
-    "block": {
-      "max_bytes": "22020096",
-      "max_gas": "-1",
-      "time_iota_ms": "10000"
-    },
-    "evidence": {
-      "max_age_num_blocks": "100000",
-      "max_age_duration": "10000"
-    },
-    "validator": {
-      "pub_key_types": [
-        "ed25519"
-      ]
-    }
-  },
-  "validators": [
-    {
-      "address": "85CD8C680EC27712EC83FEA63C16BE97115EBA97",
-      "pub_key": {
-        "type": "tendermint/PubKeyEd25519",
-        "value": "BpIfgz39GAuFSPSExT/TdJxm/UhoF1L1YxOY+pcbJzc="
+   {
+      "genesis_time":"2020-09-15T15:38:33.672114557Z",
+      "chain_id":"vocdoni-release-06",
+      "consensus_params":{
+         "block":{
+            "max_bytes":"22020096",
+            "max_gas":"-1",
+            "time_iota_ms":"10000"
+         },
+         "evidence":{
+            "max_age_num_blocks":"100000",
+            "max_age_duration":"10000"
+         },
+         "validator":{
+            "pub_key_types":[
+               "ed25519"
+            ]
+         }
       },
-      "power": "10",
-      "name": "miner4"
-    },
-    {
-      "address": "5DC922017285EC24415F3E7ECD045665EADA8B5A",
-      "pub_key": {
-        "type": "tendermint/PubKeyEd25519",
-        "value": "4MlhCW62N/5bj5tD66//h9RnsAh+xjAdMU8lGiEwvyM="
-      },
-      "power": "10",
-      "name": "miner1"
-    },
-    {
-      "address": "77EA441EA0EB29F049FC57DE524C55833A7FF575",
-      "pub_key": {
-        "type": "tendermint/PubKeyEd25519",
-        "value": "GyZfKNK3lT5AQXQ4pwrVdgG3rRisx9tS4bM9EIZ0zYY="
-      },
-      "power": "10",
-      "name": "miner2"
-    },
-    {
-      "address": "D8C253A41C7D8EE0E2AD04B2A1B6AED37FAE18E7",
-      "pub_key": {
-        "type": "tendermint/PubKeyEd25519",
-        "value": "zNYNrEVl0tGegjLgq8ZQOHUC+glzpHnmOs9x+9n9UgQ="
-      },
-      "power": "10",
-      "name": "miner3"
-    }
-  ],
-  "app_hash": "",
-  "app_state": {
-    "validators": [
-      {
-        "address": "85CD8C680EC27712EC83FEA63C16BE97115EBA97",
-        "pub_key": {
-          "type": "tendermint/PubKeyEd25519",
-          "value": "BpIfgz39GAuFSPSExT/TdJxm/UhoF1L1YxOY+pcbJzc="
-        },
-        "power": "10",
-        "name": "miner4"
-      },
-      {
-        "address": "5DC922017285EC24415F3E7ECD045665EADA8B5A",
-        "pub_key": {
-          "type": "tendermint/PubKeyEd25519",
-          "value": "4MlhCW62N/5bj5tD66//h9RnsAh+xjAdMU8lGiEwvyM="
-        },
-        "power": "10",
-        "name": "miner1"
-      },
-      {
-        "address": "77EA441EA0EB29F049FC57DE524C55833A7FF575",
-        "pub_key": {
-          "type": "tendermint/PubKeyEd25519",
-          "value": "GyZfKNK3lT5AQXQ4pwrVdgG3rRisx9tS4bM9EIZ0zYY="
-        },
-        "power": "10",
-        "name": "miner2"
-      },
-      {
-        "address": "D8C253A41C7D8EE0E2AD04B2A1B6AED37FAE18E7",
-        "pub_key": {
-          "type": "tendermint/PubKeyEd25519",
-          "value": "zNYNrEVl0tGegjLgq8ZQOHUC+glzpHnmOs9x+9n9UgQ="
-        },
-        "power": "10",
-        "name": "miner3"
+      "validators":[
+         {
+            "address":"6DB4FEE1D370907B31196B493714FC0F45C62DED",
+            "pub_key":{
+               "type":"tendermint/PubKeyEd25519",
+               "value":"R7U+HxyTrvlXccEm1sc80ww83Fpp4xg247nmpjmkYTc="
+            },
+            "power":"10",
+            "name":"miner1"
+         },
+         {
+            "address":"71AA2FEFA96447BC5AEF9FD928F3F8ED57E695CF",
+            "pub_key":{
+               "type":"tendermint/PubKeyEd25519",
+               "value":"ixI91P+MP1jiVIy1JwQqwRdZIZxsVI0WrytAzohMGCk="
+            },
+            "power":"10",
+            "name":"miner2"
+         },
+         {
+            "address":"AA9CC01B46BDD1AC9E2197BB9B84993CCDF880B2",
+            "pub_key":{
+               "type":"tendermint/PubKeyEd25519",
+               "value":"H6oEMFrNFeQemr9Kgxjq/wVk1kZQ1VE/J1wVnVJ+K9I="
+            },
+            "power":"10",
+            "name":"miner3"
+         },
+         {
+            "address":"314D17BBE991FBD3D234E5C62CFD5D0717123C95",
+            "pub_key":{
+               "type":"tendermint/PubKeyEd25519",
+               "value":"FLEg/pgdF4dZ060mved/z99p/EJePu9kSsyLnrsRNC0="
+            },
+            "power":"10",
+            "name":"miner4"
+         },
+         {
+            "address":"34B048A4A720E6B3918CF8B75CF12555080465E5",
+            "pub_key":{
+               "type":"tendermint/PubKeyEd25519",
+               "value":"aF/+WaNs5tknRMRpTPO49TJZLmDctO+JH8uckE5fTNU="
+            },
+            "power":"10",
+            "name":"miner5"
+         }
+      ],
+      "app_hash":"",
+      "app_state":{
+         "validators":[
+            {
+               "address":"6DB4FEE1D370907B31196B493714FC0F45C62DED",
+               "pub_key":{
+                  "type":"tendermint/PubKeyEd25519",
+                  "value":"R7U+HxyTrvlXccEm1sc80ww83Fpp4xg247nmpjmkYTc="
+               },
+               "power":"10",
+               "name":"miner1"
+            },
+            {
+               "address":"71AA2FEFA96447BC5AEF9FD928F3F8ED57E695CF",
+               "pub_key":{
+                  "type":"tendermint/PubKeyEd25519",
+                  "value":"ixI91P+MP1jiVIy1JwQqwRdZIZxsVI0WrytAzohMGCk="
+               },
+               "power":"10",
+               "name":"miner2"
+            },
+            {
+               "address":"AA9CC01B46BDD1AC9E2197BB9B84993CCDF880B2",
+               "pub_key":{
+                  "type":"tendermint/PubKeyEd25519",
+                  "value":"H6oEMFrNFeQemr9Kgxjq/wVk1kZQ1VE/J1wVnVJ+K9I="
+               },
+               "power":"10",
+               "name":"miner3"
+            },
+            {
+               "address":"314D17BBE991FBD3D234E5C62CFD5D0717123C95",
+               "pub_key":{
+                  "type":"tendermint/PubKeyEd25519",
+                  "value":"FLEg/pgdF4dZ060mved/z99p/EJePu9kSsyLnrsRNC0="
+               },
+               "power":"10",
+               "name":"miner4"
+            },
+            {
+               "address":"34B048A4A720E6B3918CF8B75CF12555080465E5",
+               "pub_key":{
+                  "type":"tendermint/PubKeyEd25519",
+                  "value":"aF/+WaNs5tknRMRpTPO49TJZLmDctO+JH8uckE5fTNU="
+               },
+               "power":"10",
+               "name":"miner5"
+            }
+         ],
+         "oracles":[
+            "0xc2e396d6e6ae9b12551f0c6111f9766bec926bfe",
+            "0x1a361c26e04a33effbf3bd8617b1e3e0aa6b704f"
+         ]
       }
-        ],
-    "oracles": [
-      "0x73ab4566b2e404e2be4fe9a8fb007f4014292c0c",
-	  "0x28696305c434dd4e962677d77c719ab460c73325"
-    ]
-  }
-}
-`
-
+   }
+ `
 	DevelopmentGenesis1 = `
 {
    "genesis_time":"2020-09-06T10:29:50.512370579Z",
