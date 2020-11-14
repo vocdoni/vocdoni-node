79c18b6c15796e863e8aff7617ea86b2c18e5952
p4u <pau@dabax.net>
2020-11-14 11:53:04 +0100

    docker-compose test suit update
    Execute two tests in paralel, one for encrypted-poll and the other for
    poll-vote.
    
    Signed-off-by: p4u <pau@dabax.net>

--- dockerfiles/testsuite/start_test.sh
+++ dockerfiles/testsuite/start_test.sh
@@ -5,20 +5,42 @@ ORACLE_KEY=${TESTSUITE_ORACLE_KEY:-6aae1d165dd9776c580b8fdaf8622e39c5f943c715e20
 ELECTION_SIZE=${TESTSUITE_ELECTION_SIZE:-1000}
 
 echo "### Starting test suite ###"
+docker-compose build
 docker-compose up -d
 
 sleep 5
 echo "### Waiting for test suite to be ready ###"
 for i in {1..5}; do docker-compose run test curl --fail http://gateway:9090/ping && break || sleep 5; done
 
-echo "### Running test ###"
+(
+echo "### Running test 1 ###"
 docker-compose run test timeout 300 ./vochaintest --oracleKey=$ORACLE_KEY --electionSize=$ELECTION_SIZE --gwHost ws://gateway:9090/dvote --logLevel=INFO
-RETVAL=$?
+RETVAL1=$?
+) &
 
-echo "### Post run logs ###"
-docker-compose logs --tail 300
+(
+echo "### Running test 2 ###"
+docker-compose run test timeout 300 ./vochaintest --oracleKey=$ORACLE_KEY --electionSize=$ELECTION_SIZE --gwHost ws://gateway:9090/dvote --logLevel=INFO --electionType=poll-vote
+RETVAL2=$?
+) &
+
+sleep 10
+echo "### Waiting for tests to finish ###"
+while true; do 
+	[ -n "$RETVAL1" -a -n "$RETVAL2" ] && break
+	sleep 2
+done
+
+#echo "### Post run logs ###"
+#docker-compose logs --tail 300
 
 echo "### Cleaning environment ###"
 docker-compose down -v --remove-orphans
 
-exit $RETVAL
+[ $RETVAL1 -eq 0 -a $RETVAL2 -eq 0 ] && {
+	echo "Vochain test finished correctly!"
+	exit 0
+} || {
+	echo "Vochain test failed!"
+	exit 1
+}
