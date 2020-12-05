1b40f7e94b48b886865d8ef066092772053145e4
p4u <pau@dabax.net>
2020-12-05 22:23:08 +0100

    update development genesis
    Signed-off-by: p4u <pau@dabax.net>

--- vochain/genesis.go
+++ vochain/genesis.go
@@ -143,8 +143,8 @@ var Genesis = map[string]VochainGenesis{
 		SeedNodes:         []string{"7440a5b086e16620ce7b13198479016aa2b07988@seed.dev.vocdoni.net:26656"},
 		Genesis: `
 {
-   "genesis_time":"2020-12-03T08:40:50.512370579Z",
-   "chain_id":"vocdoni-development-28",
+   "genesis_time":"2020-12-05T08:40:50.512370579Z",
+   "chain_id":"vocdoni-development-29",
    "consensus_params":{
       "block":{
          "max_bytes":"22020096",
