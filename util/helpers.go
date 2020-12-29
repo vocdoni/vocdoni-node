0a457842c6ec846a8f13496cbc5b294ca888b17e
Daniel Martí <mvdan@mvdan.cc>
2020-12-29 23:22:58 +0000

    remove some now unnecessary helper funcs
    The IsHex funcs are no longer necessary, because the API endpoints
    already receive hex-decoded bytes.
    
    The testutil funcs to convert hex strings to [32]byte can be replaced by
    a single Random32 util func, which is simpler to use too.

--- util/helpers.go
+++ util/helpers.go
@@ -1,34 +1,11 @@
 package util
 
 import (
-	"encoding/hex"
 	"fmt"
 	"io"
 	"math/rand"
-	"regexp"
-	"strconv"
 	"time"
 )
-
-var validHexRegex = regexp.MustCompile("^([0-9a-fA-F])+$")
-
-// IsHex checks if the given string contains only valid hex symbols
-func IsHex(str string) bool { return validHexRegex.MatchString(str) }
-
-// IsHexEncodedStringWithLength checks if the given string contains only valid hex symbols and have the desired length
-func IsHexEncodedStringWithLength(str string, length int) bool {
-	str = TrimHex(str)
-	return hex.DecodedLen(len(str)) == length && IsHex(str)
-}
-
-func Hex2int64(s string) int64 {
-	// base 16 for hexadecimal
-	result, err := strconv.ParseUint(TrimHex(s), 16, 64)
-	if err != nil {
-		panic(err)
-	}
-	return int64(result)
-}
 
 func TrimHex(s string) string {
 	if len(s) >= 2 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X') {
@@ -42,6 +19,14 @@ var randReader = rand.New(rand.NewSource(time.Now().UnixNano()))
 func RandomBytes(n int) []byte {
 	bytes := make([]byte, n)
 	if _, err := io.ReadFull(randReader, bytes); err != nil {
+		panic(err)
+	}
+	return bytes
+}
+
+func Random32() [32]byte {
+	var bytes [32]byte
+	if _, err := io.ReadFull(randReader, bytes[:]); err != nil {
 		panic(err)
 	}
 	return bytes
