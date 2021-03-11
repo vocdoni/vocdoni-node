40cae8e334010726a9fc03fa06e3797b38fdfa47
p4u <pau@dabax.net>
2021-03-11 11:21:19 +0100

    indexer: fix timestamp store and minor changes on database
    Signed-off-by: p4u <pau@dabax.net>

--- vochain/scrutinizer/db.go
+++ vochain/scrutinizer/db.go
@@ -1,6 +1,9 @@
 package scrutinizer
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"time"
 
 	"github.com/timshannon/badgerhold/v3"
@@ -8,16 +11,16 @@ import (
 )
 
 type Process struct {
-	ID            []byte `badgerhold:"key"`
-	EntityID      []byte `badgerhold:"index"`
+	ID            []byte `badgerholdKey:"ID"`
+	EntityID      []byte `badgerholdIndex:"EntityID"`
 	StartBlock    uint32
-	EndBlock      uint32 `badgerhold:"index"`
-	ResultsHeight uint32 `badgerhold:"index"`
+	EndBlock      uint32 `badgerholdIndex:"EndBlock"`
+	Rheight       uint32 `badgerholdIndex:"Rheight"`
 	CensusRoot    []byte
 	CensusURI     string
 	CensusOrigin  int32
-	Status        int32  `badgerhold:"index"`
-	Namespace     uint32 `badgerhold:"index"`
+	Status        int32  `badgerholdIndex:"Status"`
+	Namespace     uint32 `badgerholdIndex:"Namespace"`
 	Envelope      *models.EnvelopeType
 	Mode          *models.ProcessMode
 	VoteOpts      *models.ProcessVoteOptions
@@ -29,13 +32,42 @@ type Process struct {
 	FinalResults  bool
 }
 
+func (p Process) String() string {
+	v := reflect.ValueOf(p)
+	t := v.Type()
+	var b strings.Builder
+	b.WriteString("{")
+	for i := 0; i < t.NumField(); i++ {
+		fv := v.Field(i)
+		if fv.IsZero() {
+			// omit zero values
+			continue
+		}
+		if b.Len() > 1 {
+			b.WriteString(" ")
+		}
+		ft := t.Field(i)
+		b.WriteString(ft.Name)
+		b.WriteString(":")
+		if ft.Type.Kind() == reflect.Slice && ft.Type.Elem().Kind() == reflect.Uint8 {
+			// print []byte as hexadecimal
+			fmt.Fprintf(&b, "%x", fv.Bytes())
+		} else {
+			fv = reflect.Indirect(fv) // print *T as T
+			fmt.Fprintf(&b, "%v", fv.Interface())
+		}
+	}
+	b.WriteString("}")
+	return b.String()
+}
+
 type Entity struct {
-	ID           []byte `badgerhold:"key"`
+	ID           []byte `badgerholdKey:"ID"`
 	CreationTime time.Time
 }
 
 type Results struct {
-	ProcessID  []byte `badgerhold:"key"`
+	ProcessID  []byte `badgerholdKey:"ProcessID"`
 	Votes      []*models.QuestionResult
 	Signatures [][]byte
 }
