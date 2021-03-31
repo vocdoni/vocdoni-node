b9dba1ca4615c545e0a81c418f4735afbed26ff7
NateWilliams2 <Williams2@grinnell.edu>
2021-03-31 11:29:26 -0500

    Update router to enable search terms

--- router/voteCallbacks.go
+++ router/voteCallbacks.go
@@ -154,7 +154,8 @@ func (r *Router) getProcessList(request routerRequest) {
 		max = MaxListSize
 	}
 	processList, err := r.Scrutinizer.ProcessList(
-		request.EntityId, "",
+		request.EntityId,
+		request.SearchTerm,
 		request.Namespace,
 		request.Status,
 		request.WithResults,
@@ -347,7 +348,7 @@ func (r *Router) getEntityList(request routerRequest) {
 	if request.ListSize > MaxListSize || request.ListSize <= 0 {
 		request.ListSize = MaxListSize
 	}
-	response.EntityIDs = r.Scrutinizer.EntityList("", request.ListSize, request.From)
+	response.EntityIDs = r.Scrutinizer.EntityList(request.SearchTerm, request.ListSize, request.From)
 	request.Send(r.buildReply(request, &response))
 }
 
