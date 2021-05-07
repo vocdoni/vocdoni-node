4c61a927b85c8785be243ac478b922d109bc0782
NateWilliams2 <Williams2@grinnell.edu>
2021-05-06 21:52:30 -0400

    Benchmark: fix broken type import

--- benchmark/vochain_benchmark_test.go
+++ benchmark/vochain_benchmark_test.go
@@ -15,7 +15,7 @@ import (
 	"go.vocdoni.io/dvote/test/testcommon/testutil"
 	"go.vocdoni.io/dvote/types"
 	"go.vocdoni.io/dvote/util"
-	"go.vocdoni.io/dvote/vochain/scrutinizer"
+	sctypes "go.vocdoni.io/dvote/vochain/scrutinizer/types"
 	models "go.vocdoni.io/proto/build/go/models"
 	"google.golang.org/protobuf/proto"
 )
@@ -235,7 +235,7 @@ func voteBench(b *testing.B, cl *client.Client, s *ethereum.SignKeys,
 	}
 
 	// generate envelope votePackage
-	votePkg := &scrutinizer.VotePackage{
+	votePkg := &sctypes.VotePackage{
 		Nonce: fmt.Sprintf("%x", util.RandomHex(32)),
 		Votes: []int{1},
 	}
