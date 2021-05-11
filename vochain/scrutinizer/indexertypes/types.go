bd5ecee1ad26afc6d4290619e8611b7ebf9152c5
NateWilliams2 <Williams2@grinnell.edu>
2021-05-11 16:02:09 -0500

    Indexer: add SourceBlockHeight to Process

--- vochain/scrutinizer/indexertypes/types.go
+++ vochain/scrutinizer/indexertypes/types.go
@@ -17,25 +17,26 @@ import (
 // The scrutinizer Process data type is different from the vochain state data type
 // since it is optimized for querying purposes and not for keeping a shared consensus state.
 type Process struct {
-	ID            types.HexBytes             `badgerholdKey:"ID" json:"processId"`
-	EntityID      types.HexBytes             `badgerholdIndex:"EntityID" json:"entityId"`
-	StartBlock    uint32                     `json:"startBlock"`
-	EndBlock      uint32                     `badgerholdIndex:"EndBlock" json:"endBlock"`
-	Rheight       uint32                     `badgerholdIndex:"Rheight" json:"-"`
-	CensusRoot    types.HexBytes             `json:"censusRoot"`
-	CensusURI     string                     `json:"censusURI"`
-	CensusOrigin  int32                      `json:"censusOrigin"`
-	Status        int32                      `badgerholdIndex:"Status" json:"status"`
-	Namespace     uint32                     `badgerholdIndex:"Namespace" json:"namespace"`
-	Envelope      *models.EnvelopeType       `json:"envelopeType"`
-	Mode          *models.ProcessMode        `json:"processMode"`
-	VoteOpts      *models.ProcessVoteOptions `json:"voteOptions"`
-	PrivateKeys   []string                   `json:"-"`
-	PublicKeys    []string                   `json:"-"`
-	QuestionIndex uint32                     `json:"questionIndex"`
-	CreationTime  time.Time                  `json:"creationTime"`
-	HaveResults   bool                       `json:"haveResults"`
-	FinalResults  bool                       `json:"finalResults"`
+	ID                types.HexBytes             `badgerholdKey:"ID" json:"processId"`
+	EntityID          types.HexBytes             `badgerholdIndex:"EntityID" json:"entityId"`
+	StartBlock        uint32                     `json:"startBlock"`
+	EndBlock          uint32                     `badgerholdIndex:"EndBlock" json:"endBlock"`
+	Rheight           uint32                     `badgerholdIndex:"Rheight" json:"-"`
+	CensusRoot        types.HexBytes             `json:"censusRoot"`
+	CensusURI         string                     `json:"censusURI"`
+	CensusOrigin      int32                      `json:"censusOrigin"`
+	Status            int32                      `badgerholdIndex:"Status" json:"status"`
+	Namespace         uint32                     `badgerholdIndex:"Namespace" json:"namespace"`
+	Envelope          *models.EnvelopeType       `json:"envelopeType"`
+	Mode              *models.ProcessMode        `json:"processMode"`
+	VoteOpts          *models.ProcessVoteOptions `json:"voteOptions"`
+	PrivateKeys       []string                   `json:"-"`
+	PublicKeys        []string                   `json:"-"`
+	QuestionIndex     uint32                     `json:"questionIndex"`
+	CreationTime      time.Time                  `json:"creationTime"`
+	HaveResults       bool                       `json:"haveResults"`
+	FinalResults      bool                       `json:"finalResults"`
+	SourceBlockHeight uint64                     `json:"sourceBlockHeight"`
 }
 
 func (p Process) String() string {
