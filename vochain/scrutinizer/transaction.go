20379dcb41e004753dfd0aee0166b5a317c72f3a
NateWilliams2 <Williams2@grinnell.edu>
2021-06-07 13:20:59 -0500

    Indexer: add txIndexLock, only update tx count if new txs exist

--- vochain/scrutinizer/transaction.go
+++ vochain/scrutinizer/transaction.go
@@ -38,20 +38,24 @@ func (s *Scrutinizer) OnNewTx(blockHeight uint32, txIndex int32) {
 // indexNewTxs indexes the txs pending in the newTxPool and updates the transaction count
 // this function should only be called within Commit(), on a new block.
 func (s *Scrutinizer) indexNewTxs(txList []*indexertypes.TxReference) {
-	txCount := &indexertypes.CountStore{}
-	if err := s.db.Get(indexertypes.CountStore_Transactions, txCount); err != nil {
-		log.Errorf("could not get tx count: %v", err)
-	}
-	for i, tx := range txList {
-		// Add confirmed txs to transaction count
-		tx.Index = txCount.Count + uint64(i) + 1 // Start indexing at 1
-		if err := s.db.Insert(tx.Index, tx); err != nil {
-			log.Errorf("cannot store tx at height %d: %v", tx.Index, err)
+	if len(txList) > 0 {
+		s.txIndexLock.Lock()
+		txCount := &indexertypes.CountStore{}
+		if err := s.db.Get(indexertypes.CountStore_Transactions, txCount); err != nil {
+			log.Errorf("could not get tx count: %v", err)
 		}
-	}
-	// Store new transaction count
-	txCount.Count += uint64(len(txList))
-	if err := s.db.Upsert(indexertypes.CountStore_Transactions, txCount); err != nil {
-		log.Errorf("could not update tx count: %v", err)
+		for i, tx := range txList {
+			// Add confirmed txs to transaction count
+			tx.Index = txCount.Count + uint64(i) + 1 // Start indexing at 1
+			if err := s.db.Insert(tx.Index, tx); err != nil {
+				log.Errorf("cannot store tx at height %d: %v", tx.Index, err)
+			}
+		}
+		// Store new transaction count
+		txCount.Count += uint64(len(txList))
+		if err := s.db.Upsert(indexertypes.CountStore_Transactions, txCount); err != nil {
+			log.Errorf("could not update tx count: %v", err)
+		}
+		s.txIndexLock.Unlock()
 	}
 }
