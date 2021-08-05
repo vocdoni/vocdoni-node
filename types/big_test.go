f29773dd58ee2c8b787dd3fc1780e51691e8cbff
p4u <pau@dabax.net>
2021-08-05 13:53:43 +0200

    scrutinizer: add custom big Int implementation
    The JSON Marshaler for the standard type big.Int optputs an integer
    number instead of an string (such as "123456789") or an hexString (such
    as "0x123456). This behavior creates a problem when importing the JSON
    data to a different implementation (i.e Javascript) since it forces to
    read the field as an integer (instead of bigInt).
    
    To fix this we introduce a new custom big.Int implementation which
    Marshals always to a bigInt string.
    
    Note that this fix is widely used by projects such as go-ethereum
    (common/hexutil)
    
    Signed-off-by: p4u <pau@dabax.net>

---
+++ types/big_test.go
@@ -0,0 +1,51 @@
+package types
+
+import (
+	"encoding/json"
+	"math/big"
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+)
+
+func TestBigInt(t *testing.T) {
+	// Test basic
+	a := new(BigInt).SetUint64(100)
+	b := new(BigInt).SetUint64(200)
+
+	a.Add(a, b)
+	qt.Assert(t, a.String(), qt.DeepEquals, "300")
+
+	a.ToInt().Add(a.ToInt(), b.ToInt())
+	qt.Assert(t, a.String(), qt.DeepEquals, "500")
+
+	// Test single text Marshaling
+	j, err := a.MarshalText()
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, string(j), qt.DeepEquals, "500")
+
+	c := new(BigInt)
+	err = c.UnmarshalText([]byte("123"))
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, c.String(), qt.DeepEquals, "123")
+
+	// Test text Marshaling inside a struct
+	js := &jsonStructTest{
+		Name:   "first",
+		BigInt: new(BigInt).SetUint64(12312312312312312312),
+	}
+	data, err := json.Marshal(js)
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, string(data), qt.DeepEquals,
+		`{"name":"first","number":"12312312312312312312"}`)
+
+	// Test bytes compatibility with standard library
+	d := new(big.Int).SetInt64(456).Bytes()
+	c.SetBytes(d)
+	qt.Assert(t, c.String(), qt.DeepEquals, "456")
+}
+
+type jsonStructTest struct {
+	Name   string  `json:"name"`
+	BigInt *BigInt `json:"number"`
+}
