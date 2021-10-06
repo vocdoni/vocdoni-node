a9ddaf7dda894c35841514628bd9c98157bc5932
vdo <vdo@greyfaze.net>
2021-10-06 14:15:44 +0100

    Prevent IPFS from scanning local networks

--- ipfs/ipfsInit.go
+++ ipfs/ipfsInit.go
@@ -60,6 +60,27 @@ func doInit(out io.Writer, repoRoot string, nBitsForKeypair int) (*config.Config
 	conf.Datastore.GCPeriod = "5m"
 	conf.Discovery.MDNS.Enabled = false
 
+	// Prevent from scanning local networks which can trigger netscan alerts.
+	// See: https://github.com/ipfs/go-ipfs/issues/7985
+	conf.Swarm.AddrFilters = []string{
+		"/ip4/10.0.0.0/ipcidr/8",
+		"/ip4/100.64.0.0/ipcidr/10",
+		"/ip4/169.254.0.0/ipcidr/16",
+		"/ip4/172.16.0.0/ipcidr/12",
+		"/ip4/192.0.0.0/ipcidr/24",
+		"/ip4/192.0.2.0/ipcidr/24",
+		"/ip4/192.168.0.0/ipcidr/16",
+		"/ip4/198.18.0.0/ipcidr/15",
+		"/ip4/198.51.100.0/ipcidr/24",
+		"/ip4/203.0.113.0/ipcidr/24",
+		"/ip4/240.0.0.0/ipcidr/4",
+		"/ip6/100::/ipcidr/64",
+		"/ip6/2001:2::/ipcidr/48",
+		"/ip6/2001:db8::/ipcidr/32",
+		"/ip6/fc00::/ipcidr/7",
+		"/ip6/fe80::/ipcidr/10",
+	}
+
 	if err := fsrepo.Init(repoRoot, conf); err != nil {
 		return nil, err
 	}
