d2ed3c39e47f0422fa2b3eaba51a18467df18ddd
Gui Iribarren <gui@altermundi.net>
2021-11-04 14:39:28 +0100

    ipfssync: major refactor, implement GossipSub and drop multirpc
      This adds a PubSub service to the ps.Host that uses the GossipSub router.
      This new PubSub service is used for routing broadcast messages
      to peers, replacing the current custom implementation.
      Also, there are no more timing drifts between periodical sending of HELLOs
      and other such messages (replaced Sleeps with Tickers)
    
    drop multirpc:
    
     * rename multirpc/subpub -> ipfssync/subpub
     * update imports pointing to renamed subpub module
     * ipfssync: remove Transport interface
     * ipfssync: drop package transports, merge code into subpub package
     * cleanup unused code
    
    subpub: major refactor, drop peers.go and transport.go
    
      notable changes:
       * drop SubPubHandle, methods are now part of SubPub
       * drop ps.Peers, reimplemented with ps.Host which already tracks peers
       * renamed is.Topic -> is.GroupKey for clarity
       * receiver channel for Listen() now passes pointers, like other msg channels
       * call ps.Listen() from ps.Start() instead of the reverse
       * use discovery.Advertise instead of custom implementation
       * better (more clear) debug messages
       * rename (hello|update)Time -> (hello|update)Interval
       * fixed a critical regression introduced in dualstack support
    
      cleanups:
       * removed outdated (and noop) subpub_test.go

---
+++ ipfssync/subpub/example_test.go
@@ -0,0 +1,21 @@
+package subpub_test
+
+import (
+	"context"
+
+	"go.vocdoni.io/dvote/ipfssync/subpub"
+	"go.vocdoni.io/dvote/log"
+	"go.vocdoni.io/dvote/util"
+)
+
+func Example() {
+	log.Init("info", "stdout")
+
+	messages := make(chan *subpub.Message)
+	groupKey := []byte("test")
+	port := 6543
+	privKey := util.RandomHex(32)
+
+	sp := subpub.NewSubPub(privKey, groupKey, int32(port), false)
+	sp.Start(context.Background(), messages)
+}
