9e4657c5a6d14f8f66899ef81bf9b477c64ac559
Eduard S <edu@aragonlabs.com>
2021-11-10 11:56:32 +0100

    Bump dev chain_id

--- vochain/genesis.go
+++ vochain/genesis.go
@@ -186,8 +186,8 @@ var Genesis = map[string]VochainGenesis{
 		},
 		Genesis: `
 {
-   "genesis_time":"2021-11-06T22:43:28.668436552Z",
-   "chain_id":"vocdoni-development-52",
+   "genesis_time":"2021-11-10T09:43:28.668436552Z",
+   "chain_id":"vocdoni-development-56",
    "consensus_params":{
       "block":{
          "max_bytes":"5120000",
