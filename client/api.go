8cadb6ae8f73f8f63910bac6daee33eb9a7e9fd7
jordipainan <jordi.pinyana.paga@gmail.com>
2021-11-19 19:12:55 +0100

    census: add support for protobuf definition of weighted voting

--- client/api.go
+++ client/api.go
@@ -686,7 +686,7 @@ func (c *Client) TestSendVotes(
 	// Generate merkle proofs
 	if proofs == nil {
 		switch censusOrigin {
-		case models.CensusOrigin_OFF_CHAIN_TREE:
+		case models.CensusOrigin_OFF_CHAIN_TREE, models.CensusOrigin_OFF_CHAIN_TREE_WEIGHTED:
 			proofs, err = c.GetMerkleProofBatch(signers, root, false)
 		case models.CensusOrigin_OFF_CHAIN_CA:
 			proofs, err = c.GetCSPproofBatch(signers, caSigner, pid)
@@ -745,7 +745,7 @@ func (c *Client) TestSendVotes(
 			EncryptionKeyIndexes: keyIndexes,
 		}
 		switch censusOrigin {
-		case models.CensusOrigin_OFF_CHAIN_TREE:
+		case models.CensusOrigin_OFF_CHAIN_TREE, models.CensusOrigin_OFF_CHAIN_TREE_WEIGHTED:
 			v.Proof = &models.Proof{
 				Payload: &models.Proof_Arbo{
 					Arbo: &models.ProofArbo{
