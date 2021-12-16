122bd9a2d0d5dcb2cd4b719f40e36c38dadb36fa
NateWilliams2 <Williams2@grinnell.edu>
2021-12-15 20:53:32 -0600

    Decode resp message on submitRawTx errors

--- rpcapi/votehandlers.go
+++ rpcapi/votehandlers.go
@@ -25,11 +25,11 @@ func (r *RPCAPI) submitRawTx(request *api.APIrequest) (*api.APIresponse, error) 
 		return nil, fmt.Errorf("no reply from vochain")
 	}
 	if res.Code != 0 {
-		return nil, fmt.Errorf("%s", res.Data)
+		return nil, fmt.Errorf("%s", string(res.Data))
 	}
 	log.Debugf("broadcasting tx hash:%s", res.Hash)
 	// return nullifier or other info
-	return &api.APIresponse{Payload: fmt.Sprintf("%x", res.Data)}, nil
+	return &api.APIresponse{Payload: fmt.Sprintf("%s", string(res.Data))}, nil
 }
 
 func (a *RPCAPI) submitEnvelope(request *api.APIrequest) (*api.APIresponse, error) {
