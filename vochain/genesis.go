d9deea6ee77163800e27266142a1f4412e3f47ca
p4u <pau@dabax.net>
2022-01-31 17:03:02 +0100

    update stage genesis
    Signed-off-by: p4u <pau@dabax.net>

--- vochain/genesis.go
+++ vochain/genesis.go
@@ -305,10 +305,42 @@ var Genesis = map[string]VochainGenesis{
 		AutoUpdateGenesis: true,
 		SeedNodes: []string{
 			"588133b8309363a2a852e853424251cd6e8c5330@seed.stg.vocdoni.net:26656"},
+		CircuitsConfig: []artifacts.CircuitConfig{
+			{ // index: 0, size: 8
+				URI: "https://raw.githubusercontent.com/vocdoni/" +
+					"zk-circuits-artifacts/6afb7c22d856c8b727262b0a0ae8ab7ca534dd4e",
+				CircuitPath: "zkcensusproof/dev/8",
+				Parameters:  []int64{8},
+				LocalDir:    "./circuits",
+				ZKeyHash:    hexToBytes("0x7d09a880e14e08143bb66a6f28e13b2707afabc79693ce1613f68083fd70ac31"),
+				WitnessHash: hexToBytes("0xd021d42d79d4e10b94a5785e1c1854ddb80d087cb58ad6bb29aa8b8d541d420d"),
+				VKHash:      hexToBytes("0xf4876aa550e33de1d1f552dc38fa89f6e87e553fd05179e693f82f661cd0c6a0"),
+			},
+			{ // index: 1, size: 16
+				URI: "https://raw.githubusercontent.com/vocdoni/" +
+					"zk-circuits-artifacts/6afb7c22d856c8b727262b0a0ae8ab7ca534dd4e",
+				CircuitPath: "zkcensusproof/dev/16",
+				Parameters:  []int64{16},
+				LocalDir:    "./circuits",
+				ZKeyHash:    hexToBytes("0x2d84f9ce275f35e10b6debde1c53d74264ebb401077aaaa2ac4970e3a06eba03"),
+				WitnessHash: hexToBytes("0xe55effa3d7ee28037a073f1096012ae75d97d949320bb0dfbc26ca553b818c70"),
+				VKHash:      hexToBytes("0x0d8af5c3cc443cfbaed59b6144b1edb959daacbae085a97f74cbafbe109de2fa"),
+			},
+			{ // index: 2, size: 1024
+				URI: "https://raw.githubusercontent.com/vocdoni/" +
+					"zk-circuits-artifacts/6afb7c22d856c8b727262b0a0ae8ab7ca534dd4e",
+				CircuitPath: "zkcensusproof/dev/1024",
+				Parameters:  []int64{1024},
+				LocalDir:    "./circuits",
+				ZKeyHash:    hexToBytes("0x1cd0c9225210700d4d6307493bbe5f98554e29339daba6d9bd08a4e0e78df443"),
+				WitnessHash: hexToBytes("0x61b40e11ece8de3fbfaf27dbd984e0e0b1fa05ee72d4faa0c2be06c1d7a9b845"),
+				VKHash:      hexToBytes("0xaed892ff98ab37b877cfcb678cb5f48f1be9d09dbbaf74b5877f46b54d10f9ad"),
+			},
+		},
 		Genesis: `
 {
-   "genesis_time":"2022-01-20T20:41:19.055210151Z",
-   "chain_id":"vocdoni-stage-10",
+   "genesis_time":"2022-01-27T19:41:19.055210151Z",
+   "chain_id":"vocdoni-stage-11",
    "consensus_params":{
       "block":{
          "max_bytes":"22020096",
