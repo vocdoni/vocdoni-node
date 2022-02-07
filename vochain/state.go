ce12a567959c6eb6f5b6c7646f2830021b03943a
Andrew Chiw <randomshinichi4869@gmail.com>
2022-02-07 10:39:02 +0100

    reexport State.setTreasurer()

--- vochain/state.go
+++ vochain/state.go
@@ -503,8 +503,8 @@ func (v *State) IsOracle(addr common.Address) (bool, error) {
 	}(), nil
 }
 
-// setTreasurer saves the Treasurer address to the state
-func (v *State) setTreasurer(address common.Address) error {
+// SetTreasurer saves the Treasurer address to the state
+func (v *State) SetTreasurer(address common.Address) error {
 	v.Tx.Lock()
 	defer v.Tx.Unlock()
 	return v.Tx.DeepSet([]byte(TreasurerKey), address.Bytes(), ExtraCfg)
