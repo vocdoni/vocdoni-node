8ab779dfa4c722aaff03e86dff4a4e518bd6fac8
p4u <pau@dabax.net>
2022-04-26 19:47:18 +0200

    vochain: implements deterministic process ID type
    Introduce a new type `ProcessID` with Marshal and Unmarshal methods.
    
    The new identifier holds information about the process
    - chainID: a 6 bytes trunked hash of the blockchain identifier
    - organization: hex address of the wallet creating the process
    - census origin: kind of census used for the process (from protobuf models)
    - envelope type: flags for configuring the ballot counting mode
    - nonce: an incremental uint32 number
    
    So processID: b9180c3de924d8da6bf26964af9d7eed9e03e53415d37aa96045061300000002  represents
    - chID: vocdoni-azeno
    - addr: 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
    - csOrigin: 0x06
    - envType: 10011 (0x13)
    - nonce: 2
    
    This commit only introduces the new type, not the required changes on the blockchain logic.
    
    Discussion: https://github.com/vocdoni/protocol/discussions/28
    
    Signed-off-by: p4u <pau@dabax.net>

---
+++ vochain/process_id.go
@@ -0,0 +1,178 @@
+package vochain
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"go.vocdoni.io/dvote/crypto/ethereum"
+	"go.vocdoni.io/dvote/log"
+	"go.vocdoni.io/dvote/types"
+	"go.vocdoni.io/proto/build/go/models"
+)
+
+// ProcessID is a 32 bytes identifier that holds the following information about the voting process:
+//
+// - chainID: a 6 bytes trunked hash of the blockchain identifier
+//
+// - organization: hex address of the wallet creating the process
+//
+// - census origin: kind of census used for the process (from protobuf models)
+//
+// - envelope type: ballot properties configuration
+//
+// - nonce: an incremental uint32 number
+//
+// processID: 395b41cc9abcd8da6bf26964af9d7eed9e03e53415d37aa96045000600000001 represents
+// chID:vocdoni-azeno addr:0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045 pt:arbo nc:1
+type ProcessID struct {
+	chainID          string
+	organizationAddr common.Address
+	censusOrigin     uint8
+	envType          uint8
+	nonce            uint32
+}
+
+// Marshal encodes to bytes
+func (p *ProcessID) Marshal() []byte {
+	// processID =
+	//  chainID[6bytes] + organizationAddress[20bytes] + proofType[1byte] + envelopeType[1byte] + nonce[4bytes]
+	chainID := ethereum.HashRaw([]byte(p.chainID))
+
+	nonce := make([]byte, 4)
+	binary.BigEndian.PutUint32(nonce, uint32(p.nonce))
+
+	proofType := make([]byte, 2)
+	binary.BigEndian.PutUint16(proofType, uint16(p.censusOrigin))
+
+	envType := make([]byte, 2)
+	binary.BigEndian.PutUint16(envType, uint16(p.envType))
+
+	id := make([]byte, 32)
+	id = append(chainID[:6], p.organizationAddr.Bytes()[:20]...)
+	id = append(id, proofType[1:]...)
+	id = append(id, envType[1:]...)
+	return append(id, nonce[:4]...)
+}
+
+// MarshalBinary implements the BinaryMarshaler interface
+func (p *ProcessID) MarshalBinary() (data []byte, err error) {
+	return p.Marshal(), nil
+}
+
+// UnmarshalBinary implements the BinaryMarshaler interface
+func (p *ProcessID) UnmarshalBinary(data []byte) error {
+	return p.Unmarshal(data)
+}
+
+// Unmarshal decodes a 32 byte payload into the process ID fields
+func (p *ProcessID) Unmarshal(pid []byte) error {
+	if len(pid) != 32 {
+		return fmt.Errorf("processID lenght not correct")
+	}
+
+	p.chainID = fmt.Sprintf("%x", pid[:4])
+
+	baddr := make([]byte, 20)
+	copy(baddr[:], pid[6:26])
+	p.organizationAddr = common.BytesToAddress(baddr)
+
+	p.censusOrigin = uint8(binary.BigEndian.Uint16(append([]byte{0x00}, pid[26:27]...)))
+	if p.censusOrigin == 0 {
+		return fmt.Errorf("cannot unmarshal processID: census origin is invalid")
+	}
+
+	p.envType = uint8(binary.BigEndian.Uint16(append([]byte{0x00}, pid[27:28]...)))
+	if types.ProcessesContractMaxEnvelopeType < p.envType {
+		return fmt.Errorf("cannot unmarshal processID: overflow on envelope type %d", p.envType)
+	}
+
+	p.nonce = binary.BigEndian.Uint32(pid[28:32])
+	return nil
+}
+
+// String returns a human readable representation of process ID
+func (p *ProcessID) String() string {
+	return fmt.Sprintf("[chID:%s/addr:%s/co:%d/et:%d/nc:%d]",
+		p.chainID, p.organizationAddr.Hex(), p.censusOrigin, p.envType, p.nonce)
+
+}
+
+// SetChainID sets the blockchain identifier
+func (p *ProcessID) SetChainID(chID string) {
+	p.chainID = chID
+}
+
+// ChainID returns blockchain identifier
+func (p *ProcessID) ChainID() string {
+	return p.chainID
+}
+
+// SetAddr sets the organizer address (20 bytes)
+func (p *ProcessID) SetAddr(addr common.Address) {
+	p.organizationAddr = common.BytesToAddress(addr.Bytes())
+}
+
+// Addr returns the organizer address
+func (p *ProcessID) Addr() common.Address {
+	return common.BytesToAddress(p.organizationAddr.Bytes())
+}
+
+// SetNonce sets the nonce number that identifies the process
+func (p *ProcessID) SetNonce(nonce uint32) {
+	p.nonce = nonce
+}
+
+// Nonce returns the process ID identifier number
+func (p *ProcessID) Nonce() uint32 {
+	return p.nonce
+}
+
+// SetCensusOrigin sets the census origin type
+func (p *ProcessID) SetCensusOrigin(csOrg models.CensusOrigin) error {
+	if csOrg == 0 || csOrg > 255 {
+		return fmt.Errorf("cannot set census origin for processID: invalid value")
+	}
+	p.censusOrigin = uint8(csOrg)
+	return nil
+}
+
+// CensusOrigin returns the encoded census origin
+func (p *ProcessID) CensusOrigin() models.CensusOrigin {
+	return models.CensusOrigin(p.censusOrigin)
+}
+
+// SetEnvelopeType sets the envelope type for the process
+func (p *ProcessID) SetEnvelopeType(et *models.EnvelopeType) error {
+	if et == nil {
+		return fmt.Errorf("invalid envelope type: (%s)", log.FormatProto(et))
+	}
+	p.envType = 0
+	if et.Serial {
+		p.envType = p.envType | byte(0b00000001)
+	}
+	if et.Anonymous {
+		p.envType = p.envType | byte(0b00000010)
+	}
+	if et.EncryptedVotes {
+		p.envType = p.envType | byte(0b00000100)
+	}
+	if et.UniqueValues {
+		p.envType = p.envType | byte(0b00001000)
+	}
+	if et.CostFromWeight {
+		p.envType = p.envType | byte(0b00010000)
+	}
+	return nil
+}
+
+// EnvelopeType returns the envelope type encoded into the process ID
+func (p *ProcessID) EnvelopeType() *models.EnvelopeType {
+	return &models.EnvelopeType{
+		Serial:         p.envType&byte(0b00000001) > 0,
+		Anonymous:      p.envType&byte(0b00000010) > 0,
+		EncryptedVotes: p.envType&byte(0b00000100) > 0,
+		UniqueValues:   p.envType&byte(0b00001000) > 0,
+		CostFromWeight: p.envType&byte(0b00010000) > 0,
+	}
+}
---
+++ vochain/process_id_test.go
@@ -0,0 +1,59 @@
+package vochain
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	qt "github.com/frankban/quicktest"
+	"go.vocdoni.io/proto/build/go/models"
+)
+
+const test_vbAddr = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
+const test_chID = "vocdoni-azeno"
+
+func TestProcessID(t *testing.T) {
+	pid := ProcessID{}
+	pid.SetAddr(common.HexToAddress(test_vbAddr))
+	pid.SetNonce(1)
+	pid.SetChainID(test_chID)
+	csOrg := models.CensusOrigin_OFF_CHAIN_TREE_WEIGHTED
+	pid.SetCensusOrigin(csOrg)
+	pid.SetNonce(1)
+	pid.SetEnvelopeType(&models.EnvelopeType{Serial: true, CostFromWeight: true, EncryptedVotes: true})
+
+	et := pid.EnvelopeType()
+	qt.Assert(t, et.Serial, qt.Equals, true)
+	qt.Assert(t, et.CostFromWeight, qt.Equals, true)
+	qt.Assert(t, et.UniqueValues, qt.Equals, false)
+	qt.Assert(t, et.EncryptedVotes, qt.Equals, true)
+	qt.Assert(t, et.Anonymous, qt.Equals, false)
+
+	pid2 := ProcessID{}
+	b, err := hex.DecodeString("395b41cc9abcd8da6bf26964af9d7eed9e03e53415d37aa96045021300000002")
+	qt.Assert(t, err, qt.IsNil)
+	err = pid2.Unmarshal(b)
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, pid2.Nonce(), qt.Equals, uint32(2))
+	qt.Assert(t, pid2.CensusOrigin(), qt.Equals, models.CensusOrigin_OFF_CHAIN_TREE_WEIGHTED)
+	qt.Assert(t, pid2.envType, qt.Equals, uint8(0x13))
+
+	// 0x13 = 10011
+	et = pid2.EnvelopeType()
+	qt.Assert(t, et.Serial, qt.Equals, true)
+	qt.Assert(t, et.CostFromWeight, qt.Equals, true)
+	qt.Assert(t, et.UniqueValues, qt.Equals, false)
+	qt.Assert(t, et.EncryptedVotes, qt.Equals, false)
+	qt.Assert(t, et.Anonymous, qt.Equals, true)
+
+	csOrg = models.CensusOrigin_ERC20
+	err = pid.SetCensusOrigin(csOrg)
+	qt.Assert(t, err, qt.IsNil)
+	err = pid2.Unmarshal(pid.Marshal())
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, pid2.CensusOrigin(), qt.Equals, models.CensusOrigin_ERC20)
+	qt.Assert(t, pid2.Addr().Hex(), qt.Equals, test_vbAddr)
+
+	err = pid.SetCensusOrigin(777)
+	qt.Assert(t, err, qt.IsNotNil)
+}
