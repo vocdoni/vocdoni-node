b2345df0a19d42c426dbf2757dd2eebce884a92a
Gui Iribarren <gui@altermundi.net>
2022-07-28 15:05:36 -0300

    ci: panic when log output contains invalid chars (see #589)
    Signed-off-by: Gui Iribarren <gui@altermundi.net>

---
+++ log/log_test.go
@@ -0,0 +1,22 @@
+package log
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCheckInvalidChars(t *testing.T) {
+	v := []byte{'h', 'e', 'l', 'l', 'o', 0xff, 'w', 'o', 'r', 'l', 'd'}
+
+	_ = os.Setenv("LOG_PANIC_ON_INVALIDCHARS", "false")
+	Init("error", "stderr")
+	Debugf("%s", v)
+	// should not panic since env var is false. if it panics, test will fail
+
+	// now enable panic and try again: should recover() and never reach t.Errorf()
+	_ = os.Setenv("LOG_PANIC_ON_INVALIDCHARS", "true")
+	Init("error", "stderr")
+	defer func() { recover() }()
+	Debugf("%s", v)
+	t.Errorf("Debugf(%s) should have panicked because of invalid char", v)
+}
