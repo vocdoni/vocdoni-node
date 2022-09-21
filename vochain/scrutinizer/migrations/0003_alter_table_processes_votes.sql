b3d15f45ee12115fb1d6bf80d87e9d02662ab3c1
Daniel Martí <mvdan@mvdan.cc>
2022-09-21 11:28:50 +0100

    vochain/scrutinizer: set defaults for added NOT NULL columns
    We'll need to double check whether our code works okay with zero values,
    but at least the migration shouldn't crash due to disallowed NULL values.

--- vochain/scrutinizer/migrations/0003_alter_table_processes_votes.sql
+++ vochain/scrutinizer/migrations/0003_alter_table_processes_votes.sql
@@ -1,12 +1,12 @@
 -- +goose Up
-ALTER TABLE vote_references ADD voter_id BLOB NOT NULL;
+ALTER TABLE vote_references ADD voter_id BLOB NOT NULL DEFAULT X'';
 
 -- results columns; in badgerhold these were a separate table
-ALTER TABLE processes ADD results_votes TEXT NOT NULL;
-ALTER TABLE processes ADD results_weight TEXT NOT NULL;
-ALTER TABLE processes ADD results_envelope_height INTEGER NOT NULL;
-ALTER TABLE processes ADD results_signatures TEXT NOT NULL;
-ALTER TABLE processes ADD results_block_height INTEGER NOT NULL;
+ALTER TABLE processes ADD results_votes TEXT NOT NULL DEFAULT '';
+ALTER TABLE processes ADD results_weight TEXT NOT NULL DEFAULT '';
+ALTER TABLE processes ADD results_envelope_height INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE processes ADD results_signatures TEXT NOT NULL DEFAULT '';
+ALTER TABLE processes ADD results_block_height INTEGER NOT NULL DEFAULT 0;
 
 -- +goose Down
 ALTER TABLE vote_references DROP COLUMN voter_id;
