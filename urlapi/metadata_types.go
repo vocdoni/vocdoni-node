1e6d9341085212b62958204f02df9059f476c9fc
p4u <pau@dabax.net>
2022-09-27 17:55:28 +0200

    apiv2: remove some omitempty JSON descriptors
    Signed-off-by: p4u <pau@dabax.net>

--- urlapi/metadata_types.go
+++ urlapi/metadata_types.go
@@ -2,13 +2,13 @@ package urlapi
 
 // ElectionMetadata contains the process metadata fields as stored on ipfs
 type ElectionMetadata struct {
-	Description LanguageString         `json:"description,omitempty"`
+	Title       LanguageString         `json:"title"`
+	Version     string                 `json:"version"`
+	Description LanguageString         `json:"description"`
 	Media       ProcessMedia           `json:"media,omitempty"`
 	Meta        interface{}            `json:"meta,omitempty"`
 	Questions   []Question             `json:"questions,omitempty"`
 	Results     ElectionResultsDetails `json:"results,omitempty"`
-	Title       LanguageString         `json:"title,omitempty"`
-	Version     string                 `json:"version,omitempty"`
 }
 
 // LanguageString is a wrapper for multi-language strings, specified in metadata.
@@ -24,8 +24,8 @@ type ProcessMedia struct {
 // ElectionResultsDetails describes how a process results should be displayed and aggregated
 // Remove?
 type ElectionResultsDetails struct {
-	Aggregation string `json:"aggregation,omitempty"`
-	Display     string `json:"display,omitempty"`
+	Aggregation string `json:"aggregation"`
+	Display     string `json:"display"`
 }
 
 // Question contains metadata for one single question of a process
@@ -37,24 +37,24 @@ type Question struct {
 
 // ChoiceMetadata contains metadata for one choice of a question
 type ChoiceMetadata struct {
-	Title LanguageString `json:"title,omitempty"`
-	Value uint32         `json:"value,omitempty"`
+	Title LanguageString `json:"title"`
+	Value uint32         `json:"value"`
 }
 
-// EntityMetadata is the metadata for an organization
-type EntityMetadata struct {
-	Version     string         `json:"version,omitempty"`
-	Languages   []string       `json:"languages,omitempty"`
-	Name        LanguageString `json:"name,omitempty"`
-	Description LanguageString `json:"description,omitempty"`
-	NewsFeed    LanguageString `json:"newsFeed,omitempty"`
-	Media       EntityMedia    `json:"media,omitempty"`
-	Meta        interface{}    `json:"meta,omitempty"`
-	Actions     interface{}    `json:"actions,omitempty"`
+// OrganizationMetadata is the metadata for an organization
+type OrganizationMetadata struct {
+	Version     string            `json:"version,omitempty"`
+	Languages   []string          `json:"languages,omitempty"`
+	Name        LanguageString    `json:"name,omitempty"`
+	Description LanguageString    `json:"description,omitempty"`
+	NewsFeed    LanguageString    `json:"newsFeed,omitempty"`
+	Media       OrganizationMedia `json:"media,omitempty"`
+	Meta        interface{}       `json:"meta,omitempty"`
+	Actions     interface{}       `json:"actions,omitempty"`
 }
 
-// EntityMedia stores the avatar, header, and logo for an entity metadata
-type EntityMedia struct {
+// OrganizationMedia stores the avatar, header, and logo for an entity metadata
+type OrganizationMedia struct {
 	Avatar string `json:"avatar,omitempty"`
 	Header string `json:"header,omitempty"`
 	Logo   string `json:"logo,omitempty"`
