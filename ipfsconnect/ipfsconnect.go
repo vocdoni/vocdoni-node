49d9685b7e726a55ede93d408eba1370df6afc65
Gui Iribarren <gui@altermundi.net>
2022-11-22 11:09:42 -0300

    drop pinning code

--- ipfsconnect/ipfsconnect.go
+++ ipfsconnect/ipfsconnect.go
@@ -2,12 +2,9 @@
 package ipfssync
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"net"
-	"os"
-	"path"
 	"strings"
 	"sync"
 	"time"
@@ -21,7 +18,6 @@ import (
 
 	"go.vocdoni.io/dvote/data"
 	"go.vocdoni.io/dvote/log"
-	"go.vocdoni.io/dvote/statedblegacy/gravitonstate"
 )
 
 const (
@@ -84,112 +80,6 @@ func NewIPFSsync(dataDir, groupKey, privKeyHex, transport string, storage data.S
 	return is
 }
 
-// myPins return the list of local stored pins
-func (is *IPFSsync) myPins() (pins []string, err error) {
-	ctx, cancel := context.WithTimeout(context.Background(), is.Timeout)
-	defer cancel()
-	list, err := is.Storage.ListPins(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("myPins: %w", err)
-	}
-	for i := range list {
-		pins = append(pins, i)
-	}
-	return pins, nil
-}
-
-// updateLocalPins gets the local IPFS pin list and add them to the Merkle Tree
-func (is *IPFSsync) updateLocalPins() {
-	pins, err := is.myPins()
-	if err != nil {
-		log.Errorf("updateLocalPins: %v", err)
-	}
-	for _, p := range pins {
-		is.hashTree.Add([]byte(p), []byte{}) // errror is ignored, should be fine
-	}
-	is.state.Commit()
-}
-
-// addPins adds to the MerkleTree the new pins and updates the Root
-func (is *IPFSsync) addPins(pins []*models.IpfsPin) error {
-	currentRoot := is.hashTree.Hash()
-	for _, v := range pins {
-		if len(v.Uri) > gravitonstate.GravitonMaxKeySize {
-			log.Warnf("CID exceeds the max size (got %d)", len(v.Uri))
-			continue
-		}
-		if err := is.hashTree.Add([]byte(v.Uri), []byte{}); err != nil {
-			log.Warnf("cannot add pin %s", v.Uri)
-		}
-		log.Debugf("added pin %s", v.Uri)
-	}
-	if !bytes.Equal(currentRoot, is.hashTree.Hash()) {
-		is.lastHash = currentRoot
-	}
-	_, err := is.state.Commit()
-	return err
-}
-
-func (is *IPFSsync) getMyPins() []string {
-	// Note that we return []string instead of [][]byte since we need to
-	// make copies of the keys; the key parameter in the callback isn't safe
-	// for use after the callback returns, nor can it be modified.
-	// We could make []byte copies, but since all users want strings, this
-	// is easier.
-	var mkPins []string
-	is.hashTree.Iterate(nil, func(key, value []byte) bool {
-		mkPins = append(mkPins, string(key))
-		return false
-	})
-	return mkPins
-}
-
-// syncPins get the list of pins stored in the merkle tree and pin all of them
-func (is *IPFSsync) syncPins() error {
-	mkPins := is.getMyPins()
-	ctx, cancel := context.WithTimeout(context.Background(), is.Timeout)
-	defer cancel()
-	pins, err := is.Storage.ListPins(ctx)
-	if err != nil {
-		return fmt.Errorf("syncPins: %w", err)
-	}
-	for _, pin := range mkPins {
-		if _, e := pins[pin]; e {
-			continue
-		}
-
-		log.Infof("pinning %s", pin)
-		if err := is.Storage.Pin(ctx, pin); err != nil {
-			return fmt.Errorf("syncPins: %w", err)
-		}
-	}
-	return nil
-}
-
-func (is *IPFSsync) askPins(address string, hash []byte) error {
-	var msg models.IpfsSync
-	msg.Msgtype = models.IpfsSync_FETCH
-	msg.Address = is.Transport.Address()
-	msg.Hash = hash
-	return is.unicastMsg(address, &msg)
-}
-
-func (is *IPFSsync) sendPins(address string, theirHash []byte) error {
-	var msg models.IpfsSync
-	var err error
-	msg.Msgtype = models.IpfsSync_FETCHREPLY
-	msg.Address = is.Transport.Address()
-	msg.Hash = is.hashTree.Hash()
-	msg.PinList, err = is.listPins(theirHash)
-	if err != nil {
-		return fmt.Errorf("sendPins: %w", err)
-	}
-	if len(msg.PinList) == 0 {
-		return nil
-	}
-	return is.unicastMsg(address, &msg)
-}
-
 func (is *IPFSsync) broadcastMsg(imsg *models.IpfsSync) error {
 	imsg.Timestamp = uint32(time.Now().Unix())
 	d, err := proto.Marshal(imsg)
@@ -202,10 +92,7 @@ func (is *IPFSsync) broadcastMsg(imsg *models.IpfsSync) error {
 	return nil
 }
 
-// Handle handles a Message in a thread-safe way:
-// is.updateLock RWMutex syncs the calls to:
-// * addPins which modifies IPFS pin list
-// * askPins and sendPins, which produce outgoing unicasts messages with thread-safe is.unicastMsg()
+// Handle handles a Message in a thread-safe way
 func (is *IPFSsync) Handle(msg *models.IpfsSync) error {
 	if msg.Address == is.Transport.Address() {
 		return nil
@@ -240,61 +127,8 @@ func (is *IPFSsync) Handle(msg *models.IpfsSync) error {
 			is.Storage.Node.PeerHost.ConnManager().Protect(peerInfo.ID, "ipfsPeer")
 			return is.Storage.Node.PeerHost.Connect(context.Background(), *peerInfo)
 		}
-
-	case models.IpfsSync_UPDATE:
-		if is.OnlyConnect {
-			return nil
-		}
-		if len(msg.Hash) == gravitonstate.GravitonHashSizeBytes && len(msg.Address) > 31 {
-			is.updateLock.RLock()
-			defer is.updateLock.RUnlock()
-			tree := is.state.TreeWithRoot(msg.Hash)
-			if tree == nil && !bytes.Equal(is.lastHash, msg.Hash) {
-				log.Infof("found new hash %x from %s", msg.Hash, msg.Address)
-				return is.askPins(msg.Address, is.hashTree.Hash())
-			}
-		}
-
-	// received a fetchReply, adding new pins
-	case models.IpfsSync_FETCHREPLY:
-		if is.OnlyConnect {
-			return nil
-		}
-		if len(msg.Hash) == gravitonstate.GravitonHashSizeBytes && len(msg.Address) > 31 {
-			is.updateLock.Lock()
-			defer is.updateLock.Unlock()
-			if !bytes.Equal(msg.Hash, is.hashTree.Hash()) {
-				log.Infof("got new pin list %x from %s", msg.Hash, msg.Address)
-				return is.addPins(msg.PinList)
-			}
-		}
-
-	case models.IpfsSync_FETCH:
-		if is.OnlyConnect {
-			return nil
-		}
-		if len(msg.Address) > 31 {
-			is.updateLock.RLock()
-			defer is.updateLock.RUnlock()
-			log.Infof("got fetch query, sending pin list to %s", msg.Address)
-			return is.sendPins(msg.Address, msg.Hash)
-		}
 	}
 	return nil
-}
-
-func (is *IPFSsync) sendUpdate() {
-	var msg models.IpfsSync
-	msg.Msgtype = models.IpfsSync_UPDATE
-	msg.Address = is.Transport.Address()
-	msg.Hash = is.hashTree.Hash()
-	if s := is.hashTree.Count(); s > 0 {
-		log.Infof("[ipfsSync info] pins:%d hash:%x", s, msg.Hash)
-		err := is.broadcastMsg(&msg)
-		if err != nil {
-			log.Warnf("sendUpdate: %v", err)
-		}
-	}
 }
 
 func (is *IPFSsync) sendHelloWithAddr(multiaddress string) {
@@ -333,21 +167,6 @@ func (is *IPFSsync) ipfsAddrs() (maddrs []multiaddr.Multiaddr) {
 	return maddrs
 }
 
-// listPins return the current pins of the Merkle Tree
-// if fromHash is a valid hash, returns only the difference between the root and the provided hash
-func (is *IPFSsync) listPins(fromHash []byte) ([]*models.IpfsPin, error) {
-	var pins []*models.IpfsPin
-	diff, err := is.state.KeyDiff(fromHash, is.hashTree.Hash())
-	if err != nil {
-		return nil, fmt.Errorf("listPins, failed KeyDiff: %w", err)
-	}
-	for _, c := range diff {
-		pins = append(pins, &models.IpfsPin{Uri: string(c)})
-	}
-	log.Debugf("listPins: sending %d pins out of %d", len(diff), is.hashTree.Count())
-	return pins, nil
-}
-
 func (is *IPFSsync) unicastMsg(address string, imsg *models.IpfsSync) error {
 	var msg subpub.Message
 	imsg.Timestamp = uint32(time.Now().Unix())
@@ -363,27 +182,6 @@ func (is *IPFSsync) unicastMsg(address string, imsg *models.IpfsSync) error {
 
 // Start initializes and start an IPFSsync instance
 func (is *IPFSsync) Start() {
-	var err error
-
-	if !is.OnlyConnect {
-		// Init pin storage
-		log.Infof("initializing new pin storage")
-		dbDir := path.Join(is.DataDir, "db")
-		if err := os.RemoveAll(dbDir); err != nil {
-			log.Fatal(err)
-		}
-		is.state = &gravitonstate.GravitonState{}
-		if err = is.state.Init(dbDir, "disk"); err != nil {
-			log.Fatal(err)
-		}
-		if err = is.state.AddTree("ipfsSync"); err != nil {
-			log.Fatal(err)
-		}
-		is.hashTree = is.state.Tree("ipfsSync")
-		is.updateLocalPins()
-		log.Infof("current hash %x", is.hashTree.Hash())
-		// end Init pin storage
-	}
 	// Init SubPub
 	is.Transport = subpub.NewSubPub(is.PrivKey, []byte(is.GroupKey), int32(is.Port), is.private)
 	is.Transport.BootNodes = is.Bootnodes
@@ -400,13 +198,7 @@ func (is *IPFSsync) handleEvents() {
 	helloTicker := time.NewTicker(is.HelloInterval)
 	defer helloTicker.Stop()
 
-	updateTicker := time.NewTicker(is.UpdateInterval)
-	defer updateTicker.Stop()
-
-	syncPinsTicker := time.NewTicker(time.Second * 10)
-	defer syncPinsTicker.Stop()
-
-	for {
+		for {
 		select {
 		case d := <-is.Messages:
 			// receive unicast & broadcast messages and handle them
@@ -421,25 +213,5 @@ func (is *IPFSsync) handleEvents() {
 		case <-helloTicker.C:
 			// send hello messages
 			is.sendHello()
-
-		case <-updateTicker.C:
-			if is.OnlyConnect {
-				continue
-			}
-			// send update messages
-			is.updateLock.Lock()
-			is.updateLocalPins()
-			is.sendUpdate()
-			is.updateLock.Unlock()
-
-		case <-syncPinsTicker.C:
-			if is.OnlyConnect {
-				continue
-			}
-			// pin everything found in the merkle tree
-			if err := is.syncPins(); err != nil {
-				log.Warnf("syncPins: %v", err)
-			}
-		}
 	}
 }
