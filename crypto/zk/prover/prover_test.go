6eae14eb2c3f311d3ffabeefa4ddb44ce6d4fd81
Lucas Menendez <hi@lucasmenendez.me>
2022-12-16 10:30:57 +0100

    refactor test using quicktest

--- crypto/zk/prover/prover_test.go
+++ crypto/zk/prover/prover_test.go
@@ -3,8 +3,9 @@ package prover
 import (
 	"log"
 	"os"
-	"reflect"
 	"testing"
+
+	qt "github.com/frankban/quicktest"
 )
 
 var (
@@ -33,69 +34,65 @@ func getExampleFile(path string) []byte {
 func Test_calcWitness(t *testing.T) {
 	// Empty and first set of valid parameters
 	var emptyWasm, emptyInputs = []byte{}, []byte{}
-	if _, err := calcWitness(emptyWasm, inputs); err == nil {
-		t.Error("expected error raised by empty circuit provided, got nil")
-	} else if _, err = calcWitness(wasm, emptyInputs); err == nil {
-		t.Error("expected error raised by empty inputs provided, got nil")
-	} else if wtns, err := calcWitness(wasm, inputs); err != nil {
-		t.Errorf("expected nil, got error %v", err)
-	} else if len(wtns) == 0 {
-		t.Error("expected not empty witness result, got empty []byte")
-	}
+
+	_, err := calcWitness(emptyWasm, inputs)
+	qt.Assert(t, err, qt.IsNotNil)
+
+	_, err = calcWitness(wasm, emptyInputs)
+	qt.Assert(t, err, qt.IsNotNil)
+
+	wtns, err := calcWitness(wasm, inputs)
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, wtns, qt.Not(qt.HasLen), 0)
 
 	// Wrong and second set of valid parameters
-	if _, err := calcWitness(wasm, inputs2); err == nil {
-		t.Error("expected error raised by wrong inputs (2) for the provided circuit (1), got nil")
-	} else if _, err = calcWitness(wasm2, inputs); err == nil {
-		t.Error("expected error raised by wrong inputs (1) for the provided circuit (2), got nil")
-	} else if wtns, err := calcWitness(wasm2, inputs2); err != nil {
-		t.Errorf("expected nil, got error %v", err)
-	} else if len(wtns) == 0 {
-		t.Error("expected not empty witness result, got empty []byte")
-	}
+	_, err = calcWitness(wasm, inputs2)
+	qt.Assert(t, err, qt.IsNotNil)
+
+	_, err = calcWitness(wasm2, inputs)
+	qt.Assert(t, err, qt.IsNotNil)
+
+	wtns, err = calcWitness(wasm2, inputs2)
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, wtns, qt.Not(qt.HasLen), 0)
 }
 
 func TestProve(t *testing.T) {
 	// Empty and valid parameters
-	var emptyZkey = []byte{}
-	if _, _, err := Prove(emptyZkey, wasm, inputs); err == nil {
-		t.Error("expected error raised by empty zkey provided, got nil")
-	} else if _, resPubSignals, err := Prove(zkey, wasm, inputs); err != nil {
-		t.Errorf("expected nil, got error %v", err)
-	} else if !reflect.DeepEqual(pubSignals, resPubSignals) {
-		t.Errorf("expected %v, got %v", string(pubSignals), string(resPubSignals))
-	}
+	_, _, err := Prove([]byte{}, wasm, inputs)
+	qt.Assert(t, err, qt.IsNotNil)
+
+	_, resPubSignals, err := Prove(zkey, wasm, inputs)
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, resPubSignals, qt.ContentEquals, pubSignals)
 
 	// Second set of valid parameters
-	if _, resPubSignals, err := Prove(zkey2, wasm2, inputs2); err != nil {
-		t.Errorf("expected nil, got error %v", err)
-	} else if !reflect.DeepEqual(pubSignals2, resPubSignals) {
-		t.Errorf("expected %v, got %v", string(pubSignals), string(resPubSignals))
-	}
+	_, resPubSignals, err = Prove(zkey2, wasm2, inputs2)
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, resPubSignals, qt.ContentEquals, pubSignals2)
 }
 
 func TestVerify(t *testing.T) {
 	// Check a valid case
-	var validProofData, validPublicSignals, _ = Prove(zkey, wasm, inputs)
-	if err := Verify(vkey, validProofData, validPublicSignals); err != nil {
-		t.Errorf("expected got nil (valid proof), got %v", err)
-	}
+	validProofData, validPublicSignals, _ := Prove(zkey, wasm, inputs)
+	err := Verify(vkey, validProofData, validPublicSignals)
+	qt.Assert(t, err, qt.IsNil)
 
 	// Check an invalid case with empty parameters
-	var emptyVkey, emptyProofData, emptyPublicSignals = []byte{}, []byte{}, []byte{}
-	if err := Verify(emptyVkey, validProofData, validPublicSignals); err == nil {
-		t.Errorf("expected error raised by empty verification key, got error %v", err)
-	} else if err = Verify(vkey, emptyProofData, validPublicSignals); err == nil {
-		t.Errorf("expected error raised by empty proof, got error %v", err)
-	} else if err = Verify(vkey, validProofData, emptyPublicSignals); err == nil {
-		t.Errorf("expected error raised by empty public signals, got error %v", err)
-	}
+	err = Verify([]byte{}, validProofData, validPublicSignals)
+	qt.Assert(t, err, qt.IsNotNil)
+
+	err = Verify(vkey, []byte{}, validPublicSignals)
+	qt.Assert(t, err, qt.IsNotNil)
+
+	err = Verify(vkey, validProofData, []byte{})
+	qt.Assert(t, err, qt.IsNotNil)
 
 	// Check a proof generated with a different zkey
-	var invalidProofData, invalidPublicSignals, _ = Prove(zkey2, wasm, inputs)
-	if err := Verify(vkey, invalidProofData, invalidPublicSignals); err == nil {
-		t.Errorf("expected error raised by a zkey not associated with the circuit or the vkey, got error %v", err)
-	} else if err := Verify(vkey2, invalidProofData, invalidPublicSignals); err == nil {
-		t.Errorf("expected error raised by a zkey not associated with the circuit but associated with the vkey, got error %v", err)
-	}
+	invalidProofData, invalidPublicSignals, _ := Prove(zkey2, wasm, inputs)
+	err = Verify(vkey, invalidProofData, invalidPublicSignals)
+	qt.Assert(t, err, qt.IsNotNil)
+
+	err = Verify(vkey2, invalidProofData, invalidPublicSignals)
+	qt.Assert(t, err, qt.IsNotNil)
 }
