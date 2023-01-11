4cefaeda2e8620a5bb87d6d7b544f4f989803610
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2023-01-11 12:02:04 +0100

    api: add scrutiny endpoint (#740)
    Add new endpoint: `GET elections/<electionId>/scrutiny` that returns the election results with some extra useful information.

--- api/helpers.go
+++ api/helpers.go
@@ -1,11 +1,15 @@
 package api
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"math/big" // required for evm encoding
 	"reflect"
 	"strings"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/iancoleman/strcase"
 	"go.vocdoni.io/proto/build/go/models"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -104,4 +108,30 @@ func convertKeysToCamel(j json.RawMessage) json.RawMessage {
 
 	}
 	return json.RawMessage(b)
+}
+
+// encodeEVMResultsArgs encodes the arguments for the EVM mimicking the Solidity built-in abi.encode(args...)
+// in this case we encode the organizationId the censusRoot and the results that will be translated in the EVM
+// contract to the corresponding struct{address, bytes32, uint256[][]}
+func encodeEVMResultsArgs(electionId common.Hash,
+	organizationId common.Address,
+	censusRoot common.Hash,
+	sourceContractAddress common.Address,
+	results [][]*big.Int,
+) (string, error) {
+	address, _ := abi.NewType("address", "", nil)
+	bytes32, _ := abi.NewType("bytes32", "", nil)
+	uint256SliceNested, _ := abi.NewType("uint256[][]", "", nil)
+	args := abi.Arguments{
+		{Type: bytes32},
+		{Type: address},
+		{Type: bytes32},
+		{Type: address},
+		{Type: uint256SliceNested},
+	}
+	abiEncodedResultsBytes, err := args.Pack(electionId, organizationId, censusRoot, sourceContractAddress, results)
+	if err != nil {
+		return "", fmt.Errorf("error encoding abi: %w", err)
+	}
+	return fmt.Sprintf("0x%s", hex.EncodeToString(abiEncodedResultsBytes)), nil
 }
