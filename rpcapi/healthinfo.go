c80948049ea9a4552442c4f46bf62364f99ea69a
Daniel Martí <mvdan@mvdan.cc>
2023-01-12 15:40:53 +0000

    all: apply Go 1.19's gofmt
    It enforces better formatting on godocs.

--- rpcapi/healthinfo.go
+++ rpcapi/healthinfo.go
@@ -33,14 +33,19 @@ func (a *RPCAPI) info(request *api.APIrequest) (*api.APIresponse, error) {
 // Health is a number between 0 and 99 that represents the status of the node, as bigger the better
 // The formula ued to calculate health is: 100* (1- ( Sum(weight[0..1] * value/value_max) ))
 // Weight is a number between 0 and 1 used to give a specific weight to a value.
-//  The sum of all weights used must be equal to 1 so 0.2*value1 + 0.8*value2 would give 20% of
-//  weight to value1 and 80% of weight to value2
+//
+//	The sum of all weights used must be equal to 1 so 0.2*value1 + 0.8*value2 would give 20% of
+//	weight to value1 and 80% of weight to value2
+//
 // Each value must be represented as a number between 0 and 1. To this aim the value might be
-//  divided by its maximum value so if the mettered value is cpuLoad, a maximum must be defined
-//  in order to give a normalized value between 0 and 1 i.e cpuLoad=2 and maxCpuLoad=10.
-//  The value is: 2/10 (where cpuLoad<10) = 0.2
+//
+//	divided by its maximum value so if the mettered value is cpuLoad, a maximum must be defined
+//	in order to give a normalized value between 0 and 1 i.e cpuLoad=2 and maxCpuLoad=10.
+//	The value is: 2/10 (where cpuLoad<10) = 0.2
+//
 // The last operation includes the reverse of the values, so 1- (result).
-//   And its *100 multiplication and trunking in order to provide a natural number between 0 and 99
+//
+//	And its *100 multiplication and trunking in order to provide a natural number between 0 and 99
 func getHealth() (int32, error) {
 	v, err := psmem.VirtualMemory()
 	if err != nil {
