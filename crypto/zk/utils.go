ac772c875299427adc0577c5733e8728a70ad375
Lucas Menendez <hi@lucasmenendez.me>
2023-01-16 12:45:17 +0100

    clean commented codeblocks, documented some new functions, create a file to store circuit config on apiclient side, fix end2end anonymous voting test

--- crypto/zk/utils.go
+++ crypto/zk/utils.go
@@ -12,56 +12,6 @@ import (
 	models "go.vocdoni.io/proto/build/go/models"
 )
 
-// func LoadVkFromFile(path string) (*types.Vk, error) {
-// 	vkJSON, err := os.ReadFile(path)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	vk, err := parsers.ParseVk(vkJSON)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return vk, nil
-// }
-
-// func ProtobufZKProofToCircomProof(p *models.ProofZkSNARK) (*types.Proof, []*big.Int, error) {
-// 	if len(p.A) != 3 || len(p.B) != 6 || len(p.C) != 3 {
-// 		return nil, nil, fmt.Errorf("error on zkProof format")
-// 	}
-// 	proofString := parsers.ProofString{
-// 		A: p.A,
-// 		B: [][]string{
-// 			{
-// 				p.B[0],
-// 				p.B[1],
-// 			},
-// 			{
-// 				p.B[2],
-// 				p.B[3],
-// 			},
-// 			{
-// 				p.B[4],
-// 				p.B[5],
-// 			},
-// 		},
-// 		C: p.C,
-// 	}
-// 	publicInputsString := p.PublicInputs
-
-// 	// parse zkProof & PublicInputs from tx.Proof
-// 	proof, err := parsers.ProofStringToProof(proofString)
-// 	if err != nil {
-// 		return nil, nil, err
-// 	}
-// 	publicInputs, err := parsers.PublicSignalsStringToPublicSignals(publicInputsString)
-// 	if err != nil {
-// 		return nil, nil, err
-// 	}
-
-// 	return proof, publicInputs, nil
-// }
-
 func ProtobufZKProofToProverProof(p *models.ProofZkSNARK) (*prover.Proof, error) {
 	if len(p.A) != 3 || len(p.B) != 6 || len(p.C) != 3 {
 		return nil, fmt.Errorf("error on zkProof format")
