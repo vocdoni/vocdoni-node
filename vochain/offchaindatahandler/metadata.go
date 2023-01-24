f22729ddb0af83c314f65ddd14aba50488f15388
Maria Davila <mariajdab@gmail.com>
2023-01-24 11:02:31 -0400

    offchaindatahandler: keeping all receiver names equal

--- vochain/offchaindatahandler/metadata.go
+++ vochain/offchaindatahandler/metadata.go
@@ -8,12 +8,12 @@ import (
 
 // enqueueMetadata enqueue a election metadata for download.
 // (safe for concurrent use, simply pushes an item to a channel)
-func (d *OffChainDataHandler) enqueueMetadata(uri string) {
-	if !strings.HasPrefix(uri, d.storage.RemoteStorage.URIprefix()) {
+func (c *OffChainDataHandler) enqueueMetadata(uri string) {
+	if !strings.HasPrefix(uri, c.storage.RemoteStorage.URIprefix()) {
 		log.Warnf("metadata URI not valid: %s", uri)
 		return
 	}
-	d.storage.AddToQueue(uri, func(s string, b []byte) {
+	c.storage.AddToQueue(uri, func(s string, b []byte) {
 		log.Infof("metadata downloaded successfully from %s (%d bytes)", s, len(b))
 	}, true)
 }
