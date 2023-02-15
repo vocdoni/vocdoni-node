e55b9dfabc1f02c8f5966e84dcc453d6fd83628c
Lucas Menendez <hi@lucasmenendez.me>
2023-02-15 10:54:51 +0100

    if/elseif cases removed to improve readability

--- crypto/zk/circuit/circuit.go
+++ crypto/zk/circuit/circuit.go
@@ -35,8 +35,7 @@ type ZkCircuit struct {
 	ProvingKey      []byte
 	VerificationKey []byte
 	Wasm            []byte
-
-	Config ZkCircuitConfig
+	Config          ZkCircuitConfig
 }
 
 // LoadZkCircuitByTag gets the circuit configuration associated to the provided
@@ -44,15 +43,12 @@ type ZkCircuit struct {
 // be used.
 func LoadZkCircuitByTag(configTag string) (*ZkCircuit, error) {
 	circuitConf := GetCircuitConfiguration(configTag)
-
 	ctx, cancel := context.WithTimeout(context.Background(), downloadCircuitsTimeout)
 	defer cancel()
-
 	zkCircuit, err := LoadZkCircuit(ctx, circuitConf)
 	if err != nil {
 		return nil, err
 	}
-
 	return zkCircuit, nil
 }
 
@@ -64,7 +60,6 @@ func LoadZkCircuit(ctx context.Context, config ZkCircuitConfig) (*ZkCircuit, err
 	// configuration
 	config.LocalDir = filepath.Join(*BaseDir, config.LocalDir)
 	circuit := &ZkCircuit{Config: config}
-
 	// load the artifacts of the provided circuit from the local storage
 	if err := circuit.LoadLocal(); err == nil {
 		// tries to verify the loaded artifacts, if it success, returns the
@@ -74,20 +69,19 @@ func LoadZkCircuit(ctx context.Context, config ZkCircuitConfig) (*ZkCircuit, err
 			return circuit, nil
 		}
 	}
-
 	// if the circuit is not available locally, tries to download from its
 	// remote location
 	if err := circuit.LoadRemote(ctx); err != nil {
 		return nil, err
 	}
-
 	// checks hashes of current files
-	if correct, err := circuit.VerifiedCircuitArtifacts(); err != nil {
+	correct, err := circuit.VerifiedCircuitArtifacts()
+	if err != nil {
 		return nil, err
-	} else if !correct {
+	}
+	if !correct {
 		return nil, fmt.Errorf("hashes from downloaded artifacts don't match the expected ones")
 	}
-
 	return circuit, nil
 }
 
@@ -96,27 +90,22 @@ func LoadZkCircuit(ctx context.Context, config ZkCircuitConfig) (*ZkCircuit, err
 // operations fails, returns an error.
 func (circuit *ZkCircuit) LoadLocal() error {
 	var err error
-
 	log.Debugw("loading circuit locally...", "localDir", circuit.Config.LocalDir)
-
 	files := map[string][]byte{
 		circuit.Config.ProvingKeyFilename:      nil,
 		circuit.Config.VerificationKeyFilename: nil,
 		circuit.Config.WasmFilename:            nil,
 	}
-
 	for filename := range files {
 		// compose files localpath
 		localPath := filepath.Join(circuit.Config.LocalDir,
 			circuit.Config.CircuitPath, filename)
-
 		// read file contents locally
 		files[filename], err = os.ReadFile(localPath)
 		if err != nil {
 			return fmt.Errorf("error reading '%s' artifact locally: %w", filename, err)
 		}
 	}
-
 	// store the content into ZkCircuit struct
 	circuit.ProvingKey = files[circuit.Config.ProvingKeyFilename]
 	circuit.VerificationKey = files[circuit.Config.VerificationKeyFilename]
@@ -129,18 +118,15 @@ func (circuit *ZkCircuit) LoadLocal() error {
 func (circuit *ZkCircuit) LoadRemote(ctx context.Context) error {
 	log.Debugw("circuit not downloaded yet, downloading...",
 		"localDir", circuit.Config.LocalDir)
-
 	baseUri, err := url.Parse(circuit.Config.URI)
 	if err != nil {
 		return err
 	}
-
 	remotePath := fmt.Sprintf("%s/%s", baseUri.String(), circuit.Config.CircuitPath)
 	localPath := filepath.Join(circuit.Config.LocalDir, circuit.Config.CircuitPath)
 	if err := os.MkdirAll(localPath, os.ModePerm); err != nil {
 		return err
 	}
-
 	files := map[string][]byte{
 		circuit.Config.ProvingKeyFilename:      nil,
 		circuit.Config.VerificationKeyFilename: nil,
@@ -159,12 +145,10 @@ func (circuit *ZkCircuit) LoadRemote(ctx context.Context) error {
 		// Also store its content into the map to update the ZkCircuit struct
 		files[filename] = file
 	}
-
 	// Store the downloaded artifacts into the ZkCircuit struct
 	circuit.ProvingKey = files[circuit.Config.ProvingKeyFilename]
 	circuit.VerificationKey = files[circuit.Config.VerificationKeyFilename]
 	circuit.Wasm = files[circuit.Config.WasmFilename]
-
 	return nil
 }
 
@@ -173,25 +157,27 @@ func (circuit *ZkCircuit) LoadRemote(ctx context.Context) error {
 func (circuit *ZkCircuit) VerifiedCircuitArtifacts() (bool, error) {
 	if circuit.ProvingKey == nil || circuit.Config.ProvingKeyHash == nil {
 		return false, fmt.Errorf("provingKey or its hash are nil")
-	} else if circuit.VerificationKey == nil || circuit.Config.VerificationKeyHash == nil {
+	}
+	if circuit.VerificationKey == nil || circuit.Config.VerificationKeyHash == nil {
 		return false, fmt.Errorf("verificationKey or its hash are nil")
-	} else if circuit.Wasm == nil || circuit.Config.WasmHash == nil {
+	}
+	if circuit.Wasm == nil || circuit.Config.WasmHash == nil {
 		return false, fmt.Errorf("wasm or its hash are nil")
 	}
-
 	filesToCheck := []struct{ hash, content []byte }{
 		{hash: circuit.Config.ProvingKeyHash, content: circuit.ProvingKey},
 		{hash: circuit.Config.VerificationKeyHash, content: circuit.VerificationKey},
 		{hash: circuit.Config.WasmHash, content: circuit.Wasm},
 	}
 	for _, file := range filesToCheck {
-		if verified, err := checkHash(file.content, file.hash); !verified {
-			return false, nil
-		} else if err != nil {
+		verified, err := checkHash(file.content, file.hash)
+		if err != nil {
 			return false, err
 		}
+		if !verified {
+			return false, nil
+		}
 	}
-
 	return true, nil
 }
 
@@ -201,15 +187,14 @@ func (circuit *ZkCircuit) VerifiedCircuitArtifacts() (bool, error) {
 func checkHash(content, expected []byte) (bool, error) {
 	if content == nil {
 		return false, fmt.Errorf("no content provided to check")
-	} else if expected == nil {
+	}
+	if expected == nil {
 		return false, fmt.Errorf("no hash provided to compare")
 	}
-
 	hash := sha256.New()
 	if _, err := hash.Write(content); err != nil {
 		return false, fmt.Errorf("error computing hash function of %s: %w", content, err)
 	}
-
 	return bytes.Equal(hash.Sum(nil), expected), nil
 }
 
@@ -219,25 +204,22 @@ func downloadFile(ctx context.Context, fileUrl string) ([]byte, error) {
 	if _, err := url.Parse(fileUrl); err != nil {
 		return nil, fmt.Errorf("error parsing the file URL provided: %w", err)
 	}
-
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fileUrl, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error creating the file request: %w", err)
 	}
-
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
-	} else if res.StatusCode != http.StatusOK {
+	}
+	if res.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("error on download file %s: http status: %d", fileUrl, res.StatusCode)
 	}
-
 	defer res.Body.Close()
 	content, err := io.ReadAll(res.Body)
 	if err != nil {
 		return nil, fmt.Errorf("error reading the file content from the http response: %w", err)
 	}
-
 	return content, nil
 }
 
@@ -246,18 +228,16 @@ func downloadFile(ctx context.Context, fileUrl string) ([]byte, error) {
 func storeFile(content []byte, dstPath string) error {
 	if content == nil {
 		return fmt.Errorf("no content provided")
-	} else if _, err := os.Stat(filepath.Dir(dstPath)); err != nil {
+	}
+	if _, err := os.Stat(filepath.Dir(dstPath)); err != nil {
 		return fmt.Errorf("destination path parent folder does not exist")
 	}
-
 	fd, err := os.Create(dstPath)
 	if err != nil {
 		return fmt.Errorf("error creating the artifact file: %w", err)
 	}
-
 	if _, err := fd.Write(content); err != nil {
 		return fmt.Errorf("error writting the artifact file: %w", err)
 	}
-
 	return nil
 }
