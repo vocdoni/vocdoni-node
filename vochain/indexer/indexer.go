6d54a959c8d43e673c736637503558a8b08a284e
maria-jose <mariajdab@gmail.com>
2023-03-20 17:45:36 -0400

    comment idx.blockTx.Rollback

--- vochain/indexer/indexer.go
+++ vochain/indexer/indexer.go
@@ -482,7 +482,10 @@ func (idx *Indexer) Rollback() {
 	defer idx.lockPool.Unlock()
 	idx.votePool = make(map[string][]*state.Vote)
 	idx.voteIndexPool = []*VoteWithIndex{}
-	idx.blockTx.Rollback()
+	//err := idx.blockTx.Rollback()
+	//if err != nil {
+	//	log.Errorw(err, "rollback: cannot rollback the tx")
+	//}
 	idx.newProcessPool = []*indexertypes.IndexerOnProcessData{}
 	idx.resultsPool = []*indexertypes.IndexerOnProcessData{}
 	idx.updateProcessPool = [][]byte{}
