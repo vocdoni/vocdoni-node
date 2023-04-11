6f0d677226636f0f2e7dec41813896722ce7c2d6
Gui Iribarren <gui@altermundi.net>
2023-04-11 13:55:27 -0300

    oracle: enqueue results txs until process.Status == RESULTS (fix #880)
    if two (or more) elections finish at the same time (same block)
    the oracle computes the results and broadcasts two (or more) SetProcess txs
    with the same nonce, leaving N-1 elections in inconsistent state.
    Hotfix this issue with a simple queue.
    
    * new methods o.enqueueProcessResult and o.resultQueueHandler
    * moved relevant code into new func sendTxSetProcessResults

--- oracle/oracle.go
+++ oracle/oracle.go
@@ -2,6 +2,9 @@ package oracle
 
 import (
 	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"go.vocdoni.io/dvote/crypto/ethereum"
@@ -19,6 +22,9 @@ import (
 type Oracle struct {
 	VochainApp *vochain.BaseApplication
 	signer     *ethereum.SignKeys
+
+	resultQueue map[string]*models.ProcessResult
+	rqLock      sync.RWMutex
 }
 
 type OracleResults struct {
@@ -34,8 +40,51 @@ func NewOracle(app *vochain.BaseApplication, signer *ethereum.SignKeys) (*Oracle
 }
 
 func (o *Oracle) EnableResults(scr *indexer.Indexer) {
+	go o.resultQueueHandler()
+
 	log.Infof("oracle results enabled")
 	scr.AddEventListener(o)
+}
+
+// enqueueProcessResult pushes procresults to o.resultQueue, to be handled by resultQueueHandler
+func (o *Oracle) enqueueProcessResult(processID types.HexBytes, procresults *models.ProcessResult) {
+	o.rqLock.Lock()
+	o.resultQueue[processID.String()] = procresults
+	o.rqLock.Unlock()
+}
+
+// resultQueueHandler runs forever, every 15 seconds inspects the whole resultQueue.
+// if a process reached RESULTS status, the item is removed from the queue
+// otherwise, it broadcasts a TxSetProcess to the network, with a fresh Nonce.
+func (o *Oracle) resultQueueHandler() {
+	o.resultQueue = make(map[string]*models.ProcessResult)
+
+	for {
+		o.rqLock.Lock()
+		for key, procresults := range o.resultQueue {
+			processID := types.HexStringToHexBytes(key)
+
+			// check vochain process status
+			vocProcessData, err := o.VochainApp.State.Process(processID, true)
+			if err != nil {
+				log.Errorf("error fetching process %x from the Vochain: %v", processID, err)
+				continue
+			}
+			log.Debugw("resultQueue loop", "pid", processID, "status", vocProcessData.Status)
+
+			if vocProcessData.Status == models.ProcessStatus_RESULTS {
+				delete(o.resultQueue, key)
+				continue
+			}
+
+			err = o.sendTxSetProcessResults(processID, procresults)
+			if err != nil {
+				log.Error(err)
+			}
+		}
+		o.rqLock.Unlock()
+		time.Sleep(time.Second * 15)
+	}
 }
 
 // OnComputeResults is called once a process result is computed by the indexer.
@@ -65,41 +114,49 @@ func (o *Oracle) OnComputeResults(results *results.Results, proc *indexertypes.P
 			results.ProcessID, vocProcessData.Status)
 		return
 	}
-	// get oracle account
-	acc, err := o.VochainApp.State.GetAccount(o.signer.Address(), false)
-	if err != nil {
-		log.Errorf("error fetching oracle account: %s", err)
-		return
-	}
-	if acc == nil {
-		log.Errorf("oracle account does not exist")
-		return
-	}
-	// create setProcessTx
-	setprocessTxArgs := &models.SetProcessTx{
-		ProcessId: results.ProcessID,
-		Results:   indexer.BuildProcessResult(results, vocProcessData.EntityId),
-		Status:    models.ProcessStatus_RESULTS.Enum(),
-		Txtype:    models.TxType_SET_PROCESS_RESULTS,
-		Nonce:     acc.Nonce,
-	}
+
+	procresults := indexer.BuildProcessResult(results, vocProcessData.EntityId)
 
 	// add the signature to the results and own address
-	setprocessTxArgs.Results.OracleAddress = o.signer.Address().Bytes()
 	signedResultsPayload := OracleResults{
 		ChainID:   o.VochainApp.ChainID(),
 		EntityID:  vocProcessData.EntityId,
 		ProcessID: results.ProcessID,
-		Results:   state.GetFriendlyResults(setprocessTxArgs.Results.GetVotes()),
+		Results:   state.GetFriendlyResults(procresults.GetVotes()),
 	}
 	resultsPayload, err := json.Marshal(signedResultsPayload)
 	if err != nil {
 		log.Warnf("cannot marshal signed results: %v", err)
 		return
 	}
-	setprocessTxArgs.Results.Signature, err = o.signer.SignEthereum(resultsPayload)
+	procresults.Signature, err = o.signer.SignEthereum(resultsPayload)
 	if err != nil {
 		log.Warnf("cannot sign results: %v", err)
+	}
+	procresults.OracleAddress = o.signer.Address().Bytes()
+
+	o.enqueueProcessResult(results.ProcessID, procresults)
+}
+
+// sendTxSetProcessResults crafts a SetProcessTx containing the passed procresults,
+// with the most up-to-date oracle Nonce available,
+// signs it and broadcasts it with SendTx
+func (o *Oracle) sendTxSetProcessResults(processID types.HexBytes, procresults *models.ProcessResult) error {
+	// get oracle account
+	acc, err := o.VochainApp.State.GetAccount(o.signer.Address(), false)
+	if err != nil {
+		return fmt.Errorf("error fetching oracle account: %s", err)
+	}
+	if acc == nil {
+		return fmt.Errorf("oracle account does not exist")
+	}
+	// create setProcessTx
+	setprocessTxArgs := &models.SetProcessTx{
+		ProcessId: processID,
+		Results:   procresults,
+		Status:    models.ProcessStatus_RESULTS.Enum(),
+		Txtype:    models.TxType_SET_PROCESS_RESULTS,
+		Nonce:     acc.Nonce,
 	}
 
 	// sign and send the transaction
@@ -109,27 +166,24 @@ func (o *Oracle) OnComputeResults(results *results.Results, proc *indexertypes.P
 			SetProcess: setprocessTxArgs,
 		},
 	}); err != nil {
-		log.Errorf("cannot marshal setProcessResults tx: %v", err)
-		return
+		return fmt.Errorf("cannot marshal setProcessResults tx: %v", err)
 	}
 	if stx.Signature, err = o.signer.SignVocdoniTx(stx.Tx, o.VochainApp.ChainID()); err != nil {
-		log.Errorf("cannot sign oracle tx: %v", err)
-		return
+		return fmt.Errorf("cannot sign oracle tx: %v", err)
 	}
 
 	txb, err := proto.Marshal(stx)
 	if err != nil {
-		log.Errorf("error marshaling setProcessResults tx: %v", err)
-		return
+		return fmt.Errorf("error marshaling setProcessResults tx: %v", err)
 	}
 	log.Debugf("broadcasting Vochain Tx: %s", log.FormatProto(setprocessTxArgs))
 
 	res, err := o.VochainApp.SendTx(txb)
 	if err != nil || res == nil {
-		log.Errorf("cannot broadcast tx: %v, res: %+v", err, res)
-		return
+		return fmt.Errorf("cannot broadcast tx: %v, res: %+v", err, res)
 	}
 	log.Infof("oracle transaction sent, hash: %x", res.Hash)
+	return nil
 }
 
 // OnOracleResults does nothing. Required for implementing the indexer EventListener interface
