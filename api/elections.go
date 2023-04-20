e7c90c161f3e9f9597cf5654647aa1388b4b780f
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2023-04-20 17:12:58 +0200

    api: elections, wrap elections list into an elections object

--- api/elections.go
+++ api/elections.go
@@ -703,7 +703,11 @@ func (a *API) electionFilterPaginatedHandler(msg *apirest.APIdata, ctx *httprout
 			VoteCount:      count,
 		})
 	}
-	data, err := json.Marshal(list)
+	data, err := json.Marshal(struct {
+		Elections []ElectionSummary `json:"elections"`
+	}{
+		Elections: list,
+	})
 	if err != nil {
 		return ErrMarshalingServerJSONFailed.WithErr(err)
 	}
