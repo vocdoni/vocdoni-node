4908880aa61166f4652ec622378d74c216eb9583
Daniel Martí <mvdan@mvdan.cc>
2023-05-23 23:03:08 +0100

    vochain/indexer: no need to hold lockPool to access the state
    The mutex is there to serialize operations on blockTx for sqlite,
    and the state has its own safety mechanisms for concurrent use.
    Grabbing a lock earlier than necessary adds contention.

--- vochain/indexer/process.go
+++ vochain/indexer/process.go
@@ -241,11 +241,12 @@ func (idx *Indexer) newEmptyProcess(pid []byte) error {
 		ResultsVotes:      encodeVotes(results.NewEmptyVotes(int(options.MaxCount), int(options.MaxValue)+1)),
 	}
 
+	idx.lockPool.Lock()
+	defer idx.lockPool.Unlock()
 	queries := idx.blockTxQueries()
 	if _, err := queries.CreateProcess(context.TODO(), procParams); err != nil {
 		return fmt.Errorf("sql create process: %w", err)
 	}
-
 	return nil
 }
 
