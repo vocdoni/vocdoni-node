7ea8af46c658a7d2df6f74780055d9a4682bcdf5
Daniel Martí <mvdan@mvdan.cc>
2023-12-14 21:40:21 +0000

    types: remove unused boolean definitions

--- types/consts.go
+++ types/consts.go
@@ -2,15 +2,6 @@ package types
 
 import "time"
 
-func Bool(b bool) *bool { return &b }
-
-// These exported variables should be treated as constants, to be used in API
-// responses which require *bool fields.
-var (
-	False = Bool(false)
-	True  = Bool(true)
-)
-
 const (
 	// The mode defines the behaviour of the vocdoninode
 
