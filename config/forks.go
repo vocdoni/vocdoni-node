dc89c454444be66466f6ba84ce4b211c6c577f30
p4u <pau@dabax.net>
2024-01-03 11:18:54 +0100

    vochain: get nullifier from proof on zk voting elections
    Ignore the voteEnvelope.Nullifier since it is duplicated.
    This change produces a soft-fork.

--- config/forks.go
+++ config/forks.go
@@ -2,7 +2,8 @@ package config
 
 // ForksCfg allows applying softforks at specified heights
 type ForksCfg struct {
-	VoceremonyForkBlock uint32
+	VoceremonyForkBlock  uint32
+	NullifierFromZkProof uint32
 }
 
 // Forks is a map of chainIDs
@@ -11,10 +12,12 @@ var Forks = map[string]*ForksCfg{
 		VoceremonyForkBlock: 217200, // estimated 2023-12-05T11:33:31.426638381Z
 	},
 	"vocdoni/STAGE/9": {
-		VoceremonyForkBlock: 250000, // estimated 2023-12-11T12:09:00.917676214Z
+		VoceremonyForkBlock:  250000, // estimated 2023-12-11T12:09:00.917676214Z
+		NullifierFromZkProof: 439000, // estimated 2024-01-03T12:09:30.009477164Z
 	},
 	"vocdoni/LTS/1.2": {
-		VoceremonyForkBlock: 400200, // estimated 2023-12-12T09:09:31.511245938Z
+		VoceremonyForkBlock:  400200, // estimated 2023-12-12T09:09:31.511245938Z
+		NullifierFromZkProof: 575800, // estimated 2024-01-03T12:09:30.009477164Z
 	},
 }
 
