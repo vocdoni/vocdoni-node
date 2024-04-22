7d4ff5d3181fc7c7c8c8c53c971e41e0de2a1f0d
Gui Iribarren <gui@altermundi.net>
2024-04-22 11:53:49 +0200

    indexer: don't drop start_block, end_block, block_count in migration 10

--- vochain/indexer/migrations/0010_processes_manually_ended.sql
+++ vochain/indexer/migrations/0010_processes_manually_ended.sql
@@ -4,7 +4,3 @@ ADD COLUMN manually_ended BOOLEAN NOT NULL DEFAULT FALSE;
 
 UPDATE processes
 SET manually_ended = end_block < (start_block+block_count);
-
-ALTER TABLE processes DROP COLUMN start_block;
-ALTER TABLE processes DROP COLUMN end_block;
-ALTER TABLE processes DROP COLUMN block_count;
