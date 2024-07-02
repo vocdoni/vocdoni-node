fa0c9ba6a424fa61f17e71dcc929daac27dcc9df
Gui Iribarren <gui@altermundi.net>
2024-07-02 17:02:11 +0200

    api: re-enable autoswag since upstream bug is fixed

--- api/api.go
+++ api/api.go
@@ -1,10 +1,7 @@
 package api
 
-////// Disabled autoswag due to https://github.com/swaggo/swag/issues/1267
-////// TODO: re-enable when a fixed swaggo/swag is released
-////// and remove the workaround done by @selankon on docs/models/models.go
-////go:generate go run go.vocdoni.io/dvote/api/autoswag
-//go:generate go run github.com/swaggo/swag/cmd/swag@v1.8.10 fmt
+//go:generate go run go.vocdoni.io/dvote/api/autoswag
+//go:generate go run github.com/swaggo/swag/cmd/swag@v1.16.3 fmt
 
 import (
 	"fmt"
