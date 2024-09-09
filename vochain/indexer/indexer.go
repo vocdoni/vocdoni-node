844d05ec83ac201e86e391c30a7046badfec66ed
Daniel Martí <mvdan@mvdan.cc>
2024-09-09 22:16:34 +0100

    vochain/indexer: update sqlc to v1.27

--- vochain/indexer/indexer.go
+++ vochain/indexer/indexer.go
@@ -37,7 +37,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-//go:generate go run github.com/sqlc-dev/sqlc/cmd/sqlc@v1.25.0 generate
+//go:generate go run github.com/sqlc-dev/sqlc/cmd/sqlc@v1.27.0 generate
 
 //go:embed migrations/*.sql
 var embedMigrations embed.FS
