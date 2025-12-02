52a58ba583260f2de6d372bbf1a46f421eeeabbd
zjumathcode <pai314159@2980.com>
2025-12-02 15:11:33 +0800

    refactor: remove obsolete // +build tag
    Signed-off-by: zjumathcode <pai314159@2980.com>

--- vochain/tmdb_default.go
+++ vochain/tmdb_default.go
@@ -1,5 +1,4 @@
 //go:build !cleveldb
-// +build !cleveldb
 
 // If -tags=cleveldb is not used, we must fall back to something included by
 // tm-db by default. goleveldb is an OK choice, since it's pure Go and it's also
